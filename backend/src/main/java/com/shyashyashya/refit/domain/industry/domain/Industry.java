29f7d32c73b38160a0b9dd2ff417c3648a7ee16a
kckc0608 <kckc0608@naver.com>
2026-01-27 15:45:21 +0900

    feat: Industry 엔티티 작성

---
+++ backend/src/main/java/com/shyashyashya/refit/domain/industry/domain/Industry.java
@@ -0,0 +1,27 @@
+package com.shyashyashya.refit.domain.industry.domain;
+
+import com.shyashyashya.refit.domain.common.domain.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "industries")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Industry extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "industry_id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(columnDefinition = "varchar(20)", nullable = false)
+    private String industryName;
+}
