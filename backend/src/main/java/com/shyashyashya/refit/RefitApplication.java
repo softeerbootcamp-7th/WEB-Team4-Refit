96a9e03585be536d50e907ef56dbc099bf3270a9
kckc0608 <kckc0608@naver.com>
2026-01-27 10:37:30 +0900

    feat: BaseEntity 작성

--- backend/src/main/java/com/shyashyashya/refit/RefitApplication.java
+++ backend/src/main/java/com/shyashyashya/refit/RefitApplication.java
@@ -2,7 +2,9 @@ package com.shyashyashya.refit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class RefitApplication {
 
