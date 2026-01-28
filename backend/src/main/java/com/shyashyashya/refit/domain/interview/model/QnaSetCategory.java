1a69b445c8daa0f79c7e70c59781b30195fb438d
kckc0608 <kckc0608@naver.com>
2026-01-28 16:28:11 +0900

    feat: Qna 엔티티 클래스 작성

---
+++ backend/src/main/java/com/shyashyashya/refit/domain/interview/model/QnaSetCategory.java
@@ -0,0 +1,60 @@
+package com.shyashyashya.refit.domain.interview.model;
+
+import com.shyashyashya.refit.domain.common.model.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "qna_set_categories")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class QnaSetCategory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "qna_set_category_id")
+    private Long id;
+
+    @Column(name = "qna_set_category_name", nullable = false, columnDefinition = "varchar(30)")
+    private String categoryName;
+
+    @Column(name = "example_question", nullable = false, columnDefinition = "varchar(200)")
+    private String exampleQuestion;
+
+    @Column(nullable = false)
+    private double cohesion;
+
+    /*
+     * Static Factory Method
+     */
+    public static QnaSetCategory create(
+            String categoryName,
+            String exampleQuestion,
+            double cohesion
+    ) {
+        return QnaSetCategory.builder()
+                .categoryName(categoryName)
+                .exampleQuestion(exampleQuestion)
+                .cohesion(cohesion)
+                .build();
+    }
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private QnaSetCategory(
+            String categoryName,
+            String exampleQuestion,
+            double cohesion
+    ) {
+        this.categoryName = categoryName;
+        this.exampleQuestion = exampleQuestion;
+        this.cohesion = cohesion;
+    }
+}
