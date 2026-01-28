b0d3c3c59ec22ab30c9844f69a4f985da3bca824
kckc0608 <kckc0608@naver.com>
2026-01-28 12:08:28 +0900

    chore: spotless 적용

--- backend/src/main/java/com/shyashyashya/refit/domain/common/dto/CommonResponse.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/common/dto/CommonResponse.java
@@ -2,11 +2,7 @@ package com.shyashyashya.refit.domain.common.dto;
 
 import com.shyashyashya.refit.domain.common.model.ResponseCode;
 
-public record CommonResponse<T>(
-        boolean isSuccess,
-        ResponseCode code,
-        String message,
-        T result) {
+public record CommonResponse<T>(boolean isSuccess, ResponseCode code, String message, T result) {
 
     public static <T> CommonResponse<T> success(ResponseCode responseCode, T result) {
         return new CommonResponse<>(true, responseCode, responseCode.getMessage(), result);
