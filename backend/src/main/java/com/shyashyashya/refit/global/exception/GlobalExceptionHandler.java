f104303ad6c8c4bfc0f085ff4e681031a76e5a1d
kckc0608 <kckc0608@naver.com>
2026-01-28 14:45:36 +0900

    feat: error logging 로직 추가

--- backend/src/main/java/com/shyashyashya/refit/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/shyashyashya/refit/global/exception/GlobalExceptionHandler.java
@@ -1,22 +1,26 @@
 package com.shyashyashya.refit.global.exception;
 
 import com.shyashyashya.refit.domain.common.dto.CommonResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<CommonResponse<Void>> handleCustomException(CustomException e) {
         ErrorCode errorCode = e.getErrorCode();
+        log.info("CustomException: [{}]: {}", errorCode.name(), errorCode.getMessage());
         var response = CommonResponse.customException(errorCode);
         return ResponseEntity.status(errorCode.getHttpStatus()).body(response);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<CommonResponse<Void>> ExceptionHandler(Exception e) {
+        log.error("Exception: [{}]: {}", e.getClass().getName(), e.getMessage());
         var response = CommonResponse.unknownException(e);
         return ResponseEntity.internalServerError().body(response);
     }
