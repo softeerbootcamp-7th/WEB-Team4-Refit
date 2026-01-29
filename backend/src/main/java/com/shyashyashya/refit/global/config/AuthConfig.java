ca221e8270d946d2e1c029ae84c689c647f9a2cf
lja3723 <lja3723@gmail.com>
2026-01-30 03:02:44 +0900

    feat: 로그인이 필요 없는 API 화이트리스트 정보 추가

---
+++ backend/src/main/java/com/shyashyashya/refit/global/config/AuthConfig.java
@@ -0,0 +1,16 @@
+package com.shyashyashya.refit.global.config;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "spring.auth")
+public class AuthConfig {
+
+    private List<String> whitelistApiUrls;
+}
