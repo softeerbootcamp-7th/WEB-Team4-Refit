4af052d27815229040bd8968ec873d81fd6f2b77
kckc0608 <kckc0608@naver.com>
2026-01-29 12:15:00 +0900

    test: InterviewValidator 검증 로직 작성

---
+++ backend/src/test/java/com/shyashyashya/refit/fixture/IndustryFixture.java
@@ -0,0 +1,8 @@
+package com.shyashyashya.refit.fixture;
+
+import com.shyashyashya.refit.domain.industry.model.Industry;
+
+public class IndustryFixture {
+
+    public static Industry TEST_INDUSTRY = Industry.create("test_industry");
+}
---
+++ backend/src/test/java/com/shyashyashya/refit/fixture/UserFixture.java
@@ -0,0 +1,25 @@
+package com.shyashyashya.refit.fixture;
+
+import com.shyashyashya.refit.domain.user.model.User;
+
+import static com.shyashyashya.refit.fixture.IndustryFixture.TEST_INDUSTRY;
+import static com.shyashyashya.refit.fixture.JobCategoryFixture.TEST_JOB_CATEGORY;
+
+public class UserFixture {
+
+    public static User TEST_USER_1 = User.create(
+            "test@email.com",
+            "test",
+            "test.com/test.png",
+            true,
+            TEST_INDUSTRY,
+            TEST_JOB_CATEGORY);
+
+    public static User TEST_USER_2 = User.create(
+            "test2@email.com",
+            "test2",
+            "test.com/test2.png",
+            true,
+            TEST_INDUSTRY,
+            TEST_JOB_CATEGORY);
+}
