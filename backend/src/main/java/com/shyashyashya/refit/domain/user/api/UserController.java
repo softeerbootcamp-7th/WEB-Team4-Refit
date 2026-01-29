4996a814221fc4f42a6a740709c171d32bc08647
lja3723 <lja3723@gmail.com>
2026-01-29 16:46:54 +0900

    feat: 사용자 컨트롤러 작성
    사용자 회원가입 API 추가

---
+++ backend/src/main/java/com/shyashyashya/refit/domain/user/api/UserController.java
@@ -0,0 +1,31 @@
+package com.shyashyashya.refit.domain.user.api;
+
+import com.shyashyashya.refit.domain.common.dto.CommonResponse;
+import com.shyashyashya.refit.domain.common.model.ResponseCode;
+import com.shyashyashya.refit.domain.user.dto.request.UserSignUpDto;
+import com.shyashyashya.refit.domain.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<CommonResponse<Long>> signUp(
+        @Valid @RequestBody UserSignUpDto userSignUpDto
+    ) {
+        Long userId = userService.signUp(userSignUpDto);
+        return ResponseEntity.ok(CommonResponse.success(ResponseCode.CREATED, userId));
+    }
+}
