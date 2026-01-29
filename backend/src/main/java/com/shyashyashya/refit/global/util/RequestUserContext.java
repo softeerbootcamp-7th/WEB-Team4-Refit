c12127f98bca870aed19c9a0f89264eed2c5a25d
lja3723 <lja3723@gmail.com>
2026-01-30 03:24:08 +0900

    refactor: RequestUserContext가 User 엔티티를 바로 반환하도록 수정

--- backend/src/main/java/com/shyashyashya/refit/global/util/RequestUserContext.java
+++ backend/src/main/java/com/shyashyashya/refit/global/util/RequestUserContext.java
@@ -1,6 +1,11 @@
 package com.shyashyashya.refit.global.util;
 
+import com.shyashyashya.refit.domain.user.model.User;
+import com.shyashyashya.refit.domain.user.repository.UserRepository;
+import com.shyashyashya.refit.global.exception.CustomException;
+import com.shyashyashya.refit.global.exception.ErrorCode;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
@@ -10,6 +15,20 @@ import org.springframework.web.context.annotation.RequestScope;
 @RequestScope // RequestScope를 사용하여 요청 당 하나의 인스턴스 생성
 @Getter
 @Setter
+@RequiredArgsConstructor
 public class RequestUserContext {
+
+    private final UserRepository userRepository;
+
     private Long userId;
+    private User user;
+
+    public User getCurrentUser() {
+        if (user == null && userId != null) {
+            user = userRepository
+                .findById(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+        }
+        return user;
+    }
 }
