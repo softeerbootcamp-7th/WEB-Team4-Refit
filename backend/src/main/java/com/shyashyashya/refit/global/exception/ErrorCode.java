99bdbbc0181b64c2866f5d058c4604a7082dafaa
lja3723 <lja3723@gmail.com>
2026-01-30 00:58:55 +0900

    feat: 사용자 회원가입 중 이메일 중복 에러 추가
    # Conflicts:
    #	backend/src/main/java/com/shyashyashya/refit/global/exception/ErrorCode.java

--- backend/src/main/java/com/shyashyashya/refit/global/exception/ErrorCode.java
+++ backend/src/main/java/com/shyashyashya/refit/global/exception/ErrorCode.java
@@ -2,6 +2,7 @@ package com.shyashyashya.refit.global.exception;
 
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.CONFLICT;
 
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
@@ -12,7 +13,9 @@ public enum ErrorCode {
     INTERVIEW_NOT_ACCESSIBLE(FORBIDDEN, "인터뷰에 접근할 수 없습니다."),
 
     INDUSTRY_NOT_FOUND(NOT_FOUND, "존재하지 않는 산업군입니다."),
-    JOB_CATEGORY_NOT_FOUND(NOT_FOUND, "존재하지 않는 직업군입니다.");
+    JOB_CATEGORY_NOT_FOUND(NOT_FOUND, "존재하지 않는 직업군입니다."),
+
+    USER_SIGNUP_EMAIL_CONFLICT(CONFLICT, "이미 사용 중인 이메일입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
