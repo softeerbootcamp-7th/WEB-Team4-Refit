366b6aeee55fe43568ccb6ce0cac25cec59c6b6f
kckc0608 <kckc0608@naver.com>
2026-01-29 11:21:37 +0900

    feat: 인터뷰 삭제 서비스 계층 작성

---
+++ backend/src/main/java/com/shyashyashya/refit/domain/interview/service/InterviewService.java
@@ -0,0 +1,32 @@
+package com.shyashyashya.refit.domain.interview.service;
+
+import com.shyashyashya.refit.domain.interview.model.Interview;
+import com.shyashyashya.refit.domain.interview.repository.InterviewRepository;
+import com.shyashyashya.refit.domain.interview.service.validator.InterviewValidator;
+import com.shyashyashya.refit.domain.user.model.User;
+import com.shyashyashya.refit.global.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.shyashyashya.refit.global.exception.ErrorCode.INTERVIEW_NOT_EXISTS;
+
+@Service
+@RequiredArgsConstructor
+public class InterviewService {
+
+    private final InterviewValidator interviewValidator;
+    private final InterviewRepository interviewRepository;
+
+    @Transactional
+    public void deleteInterview(Long interviewId) {
+        User currentUser = null; // TODO: 로그인 구현 이후 현재 로그인 유저 조회 로직 수정
+
+        Interview interview = interviewRepository.findById(interviewId)
+                .orElseThrow(() -> new CustomException(INTERVIEW_NOT_EXISTS));
+
+        interviewValidator.validateInterviewOwner(interview, currentUser);
+
+        interviewRepository.deleteById(interviewId);
+    }
+}
---
+++ backend/src/main/java/com/shyashyashya/refit/domain/interview/service/validator/InterviewValidator.java
@@ -0,0 +1,19 @@
+package com.shyashyashya.refit.domain.interview.service.validator;
+
+import com.shyashyashya.refit.domain.interview.model.Interview;
+import com.shyashyashya.refit.domain.user.model.User;
+import com.shyashyashya.refit.global.exception.CustomException;
+import org.springframework.stereotype.Component;
+
+import static com.shyashyashya.refit.global.exception.ErrorCode.INTERVIEW_NOT_ACCESSIBLE;
+
+@Component
+public class InterviewValidator {
+
+    public void validateInterviewOwner(Interview interview, User user) {
+        if (interview.getUser().equals(user)) {
+            return;
+        }
+        throw new CustomException(INTERVIEW_NOT_ACCESSIBLE);
+    }
+}
