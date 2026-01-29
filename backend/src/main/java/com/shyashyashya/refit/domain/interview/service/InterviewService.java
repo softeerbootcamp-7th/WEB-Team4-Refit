0af7e8833b23b37341c9f53e007037a819e27979
kckc0608 <kckc0608@naver.com>
2026-01-29 17:26:34 +0900

    feat: interview result status 업데이트 로직 작성

--- backend/src/main/java/com/shyashyashya/refit/domain/interview/service/InterviewService.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/interview/service/InterviewService.java
@@ -2,6 +2,7 @@ package com.shyashyashya.refit.domain.interview.service;
 
 import static com.shyashyashya.refit.global.exception.ErrorCode.INTERVIEW_NOT_FOUND;
 
+import com.shyashyashya.refit.domain.interview.dto.request.InterviewResultStatusUpdateRequest;
 import com.shyashyashya.refit.domain.interview.model.Interview;
 import com.shyashyashya.refit.domain.interview.repository.InterviewRepository;
 import com.shyashyashya.refit.domain.interview.service.validator.InterviewValidator;
@@ -28,5 +29,15 @@ public class InterviewService {
         interviewValidator.validateInterviewOwner(interview, currentUser);
 
         interviewRepository.delete(interview);
+    }
+
+    @Transactional
+    public void updateResultStatus(Long interviewId, InterviewResultStatusUpdateRequest request) {
+        User currentUser = null; // TODO: 로그인 구현 이후 현재 로그인 유저 조회 로직 수정
+
+        Interview interview =
+                interviewRepository.findById(interviewId).orElseThrow(() -> new CustomException(INTERVIEW_NOT_FOUND));
+
+        interview.updateResultStatus(request.interviewResultStatus());
     }
 }
