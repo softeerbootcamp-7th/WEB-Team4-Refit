a7cb82438bf8e409d6880a77a43c6a22b3fe5a8a
lja3723 <lja3723@gmail.com>
2026-01-29 16:53:51 +0900

    feat: 사용자 서비스 추가
    사용자 회원가입 서비스 메서드 추가

---
+++ backend/src/main/java/com/shyashyashya/refit/domain/user/service/UserService.java
@@ -0,0 +1,47 @@
+package com.shyashyashya.refit.domain.user.service;
+
+import com.shyashyashya.refit.domain.industry.repository.IndustryRepository;
+import com.shyashyashya.refit.domain.jobcategory.repository.JobCategoryRepository;
+import com.shyashyashya.refit.domain.user.dto.request.UserSignUpDto;
+import com.shyashyashya.refit.domain.user.model.User;
+import com.shyashyashya.refit.domain.user.repository.UserRepository;
+import com.shyashyashya.refit.global.exception.CustomException;
+import com.shyashyashya.refit.global.exception.ErrorCode;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final IndustryRepository industryRepository;
+    private final JobCategoryRepository jobCategoryRepository;
+    private final UserSignupConflictValidator userSignupConflictValidator;
+
+    @Transactional
+    public Long signUp(UserSignUpDto userSignUpDto) {
+        var industry = industryRepository
+                .findById(userSignUpDto.industryId())
+                .orElseThrow(() -> new CustomException(ErrorCode.INDUSTRY_ID_NOT_FOUND));
+
+        var jobCategory = jobCategoryRepository
+                .findById(userSignUpDto.jobCategoryId())
+                .orElseThrow(() -> new CustomException(ErrorCode.JOB_CATEGORY_ID_NOT_FOUND));
+
+        userSignupConflictValidator.validateEmailConflict(userSignUpDto.email());
+
+        return userRepository.save(
+            User.create(
+                userSignUpDto.email(),
+                userSignUpDto.nickname(),
+                userSignUpDto.profileImageUrl(),
+                true,
+                industry,
+                jobCategory))
+            .getId();
+    }
+}
