189e9e72dd116fc57bd8ea2c5868ccf23417f6e0
kckc0608 <kckc0608@naver.com>
2026-01-30 18:05:04 +0900

    feat: 나의 면접 질문 카테고리 리스트 조회 로직 작성

---
+++ backend/src/main/java/com/shyashyashya/refit/domain/qnaset/repository/QnaSetRepository.java
@@ -0,0 +1,19 @@
+package com.shyashyashya.refit.domain.qnaset.repository;
+
+import com.shyashyashya.refit.domain.qnaset.model.QnaSet;
+import com.shyashyashya.refit.domain.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface QnaSetRepository extends JpaRepository<QnaSet, Long> {
+
+    // TODO : queryDSL 적용
+    @Query("""
+        SELECT q
+          FROM QnaSet q
+         WHERE q.interview.user = :user
+    """)
+    List<QnaSet> findAllByUser(User user);
+}
---
+++ backend/src/main/java/com/shyashyashya/refit/domain/qnaset/service/QnaSetMyService.java
@@ -0,0 +1,48 @@
+package com.shyashyashya.refit.domain.qnaset.service;
+
+import com.shyashyashya.refit.domain.qnaset.dto.response.FrequentQnaSetCategoryResponse;
+import com.shyashyashya.refit.domain.qnaset.model.QnaSet;
+import com.shyashyashya.refit.domain.qnaset.model.QnaSetCategory;
+import com.shyashyashya.refit.domain.qnaset.repository.QnaSetRepository;
+import com.shyashyashya.refit.domain.user.model.User;
+import com.shyashyashya.refit.global.util.RequestUserContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class QnaSetMyService {
+
+    private final RequestUserContext requestUserContext;
+    private final QnaSetRepository qnaSetRepository;
+
+    public Page<FrequentQnaSetCategoryResponse> getFrequentQnaSetCategories(Pageable pageable) {
+        User requestUser = requestUserContext.getUser();
+
+        // TODO : 로직 고도화 (쿼리로 한번에 조회할 수 있도록)
+        List<QnaSet> qna = qnaSetRepository.findAllByUser(requestUser);
+        Map<QnaSetCategory, Long> qnaSetCategoryCounts = qna.stream()
+                .collect(Collectors.groupingBy(QnaSet::getQnaSetCategory, Collectors.counting()));
+
+        var sortedList = qnaSetCategoryCounts.entrySet().stream()
+                .sorted(Map.Entry.<QnaSetCategory, Long>comparingByValue(Comparator.reverseOrder())
+                        .thenComparing(entry -> entry.getKey().getCategoryName()))
+                .map((entry) ->
+                FrequentQnaSetCategoryResponse.of(entry.getKey(), entry.getValue())).toList();
+
+        int start = (int) pageable.getOffset();
+        int end = Math.min(start + pageable.getPageSize(), sortedList.size());
+
+        List<FrequentQnaSetCategoryResponse> pageContent = sortedList.subList(start, end);
+
+        return new PageImpl<>(pageContent, pageable, sortedList.size());
+    }
+}
