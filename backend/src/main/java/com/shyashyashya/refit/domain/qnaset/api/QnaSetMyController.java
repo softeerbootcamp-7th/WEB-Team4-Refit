a57d96c444a22c4136304597a81129217a715ebc
kckc0608 <kckc0608@naver.com>
2026-01-30 18:08:39 +0900

    feat: 나의 빈출 질문 카테고리 조회 컨트롤러 작성

---
+++ backend/src/main/java/com/shyashyashya/refit/domain/qnaset/api/QnaSetMyController.java
@@ -0,0 +1,32 @@
+package com.shyashyashya.refit.domain.qnaset.api;
+
+import com.shyashyashya.refit.domain.common.dto.CommonResponse;
+import com.shyashyashya.refit.domain.common.model.ResponseCode;
+import com.shyashyashya.refit.domain.qnaset.dto.response.FrequentQnaSetCategoryResponse;
+import com.shyashyashya.refit.domain.qnaset.service.QnaSetMyService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.shyashyashya.refit.domain.common.model.ResponseCode.COMMON200;
+
+@Tag(name = "Qna-Set My Controller", description = "나의 질문과 관련된 API 컨트롤러 입니다.")
+@RestController
+@RequestMapping("/qna-set/my")
+@RequiredArgsConstructor
+public class QnaSetMyController {
+
+    private final QnaSetMyService qnaSetMyService;
+
+    @GetMapping("/frequent/category")
+    public ResponseEntity<CommonResponse<Page<FrequentQnaSetCategoryResponse>>> getMyFrequentQnaSetCategories(Pageable pageable) {
+        var body = qnaSetMyService.getFrequentQnaSetCategories(pageable);
+        var response = CommonResponse.success(COMMON200, body);
+        return ResponseEntity.ok(response);
+    }
+}
