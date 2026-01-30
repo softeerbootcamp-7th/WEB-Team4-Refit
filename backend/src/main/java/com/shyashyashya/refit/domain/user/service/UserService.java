92964078e4e42442d0e23a615a3250151ca20af1
lja3723 <lja3723@gmail.com>
2026-01-30 17:21:07 +0900

    feat: 사용자 이용약관 동의하는 서비스 기능 추가
    # Conflicts:
    #	backend/src/main/java/com/shyashyashya/refit/domain/user/service/UserService.java

--- backend/src/main/java/com/shyashyashya/refit/domain/user/service/UserService.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/user/service/UserService.java
@@ -11,6 +11,7 @@ import com.shyashyashya.refit.domain.user.model.User;
 import com.shyashyashya.refit.domain.user.repository.UserRepository;
 import com.shyashyashya.refit.domain.user.service.validator.UserSignUpValidator;
 import com.shyashyashya.refit.global.exception.CustomException;
+import com.shyashyashya.refit.global.util.RequestUserContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -23,32 +24,40 @@ public class UserService {
     private final UserRepository userRepository;
     private final IndustryRepository industryRepository;
     private final JobCategoryRepository jobCategoryRepository;
+
     private final UserSignUpValidator userSignUpValidator;
+    private final RequestUserContext requestUserContext;
 
     @Transactional
     public void signUp(UserSignUpRequest userSignUpRequest) {
         var industry = industryRepository
-                .findById(userSignUpRequest.industryId())
-                .orElseThrow(() -> new CustomException(INDUSTRY_NOT_FOUND));
+            .findById(userSignUpRequest.industryId())
+            .orElseThrow(() -> new CustomException(INDUSTRY_NOT_FOUND));
 
         var jobCategory = jobCategoryRepository
-                .findById(userSignUpRequest.jobCategoryId())
-                .orElseThrow(() -> new CustomException(JOB_CATEGORY_NOT_FOUND));
+            .findById(userSignUpRequest.jobCategoryId())
+            .orElseThrow(() -> new CustomException(JOB_CATEGORY_NOT_FOUND));
 
         userSignUpValidator.validateEmailConflict(userSignUpRequest.email());
 
         var user = User.create(
-                userSignUpRequest.email(),
-                userSignUpRequest.nickname(),
-                userSignUpRequest.profileImageUrl(),
-                false,
-                industry,
-                jobCategory);
+            userSignUpRequest.email(),
+            userSignUpRequest.nickname(),
+            userSignUpRequest.profileImageUrl(),
+            false,
+            industry,
+            jobCategory);
 
         try {
             userRepository.save(user);
         } catch (DataIntegrityViolationException e) {
             throw new CustomException(USER_SIGNUP_EMAIL_CONFLICT);
         }
+    }
+
+    @Transactional
+    public void agreeToTerms()  {
+        User user = requestUserContext.getRequestUser();
+        user.agreeToTerms();
     }
 }
