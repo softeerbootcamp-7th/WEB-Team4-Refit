d250e50db6dd41a27eb362bbdfb67c72e4d106ad
zxc534 <zxc534@naver.com>
2026-01-30 17:27:40 +0900

    feat: 가이드 질문 생성 서비스 로직 작성(랜덤 생성)

--- backend/src/main/java/com/shyashyashya/refit/domain/interview/service/GuideQuestionService.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/interview/service/GuideQuestionService.java
@@ -7,6 +7,7 @@ import com.shyashyashya.refit.domain.interview.repository.InterviewRepository;
 import com.shyashyashya.refit.domain.interview.service.validator.InterviewValidator;
 import com.shyashyashya.refit.domain.user.model.User;
 import com.shyashyashya.refit.global.exception.CustomException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,11 +33,24 @@ public class GuideQuestionService {
     }
 
     @FunctionalInterface
-    interface GuideQuestionPolicy {
+    private interface GuideQuestionPolicy {
         String generate(String rawText);
     }
 
     private final GuideQuestionPolicy randomGuideQuestion = rawText -> {
-        return null;
+        int idx = (int) System.currentTimeMillis() % (GUIDE_QUESTIONS.size());
+        return GUIDE_QUESTIONS.get(idx);
     };
+
+    private static final List<String> GUIDE_QUESTIONS = List.of(
+            "입사 후 포부가 무엇입니까?",
+            "입사 5년 후, 10년 후 자신의 모습은 어떨 것이라고 생각합니까?",
+            "본인의 직업관은 무엇입니까?",
+            "당신에게 일이 왜 중요합니까?",
+            "직장은 어떤 면을 보고 선택합니까?",
+            "일하는 목적이 무엇입니까?",
+            "어떤 회사가 훌륭한 회사라고 생각합니까?",
+            "인생에서 가장 필요한 사항은 무엇이라 생각하나요?",
+            "많은 직무 중 이 직무를 선택한 이유는 무엇입니까?",
+            "바람직한 사원상은 무엇이라고 생각합니까?");
 }
