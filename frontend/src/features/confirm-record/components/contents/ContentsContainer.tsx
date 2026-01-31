ff642478453ead72185fa02a61abc8236d7b201a
HIHJH <2170095@ewhain.net>
2026-02-01 06:18:53 +0900

    feat: toggle 컴포넌트 버튼 태그 추가 및 내부에서 상태 관리하도록 변경

--- frontend/src/features/confirm-record/components/contents/ContentsContainer.tsx
+++ frontend/src/features/confirm-record/components/contents/ContentsContainer.tsx
@@ -4,36 +4,44 @@ import { Border } from '@/shared/sidebar/Border'
 
 type QnAType = {
   question: string
+  questionId: number
   answer: string
 }
-export const ContentsContainer = ({ data, idx }: { data: QnAType; idx: number }) => {
-  const [isActive, setIsActive] = useState(false)
 
-  const handleHardToggleClick = () => {
-    setIsActive((prev) => !prev)
-  }
+type ContentsContainerProps = {
+  qnaData: QnAType
+  idx: number
+}
+
+export const ContentsContainer = ({ qnaData, idx }: ContentsContainerProps) => {
   return (
     <div className="bg-gray-white flex flex-col gap-4 rounded-lg p-5">
       <div className="inline-flex flex-wrap gap-2.5">
         <Badge type="question-label" theme="orange-100" content={`${idx}번 질문`} />
-        <span className="title-m-semibold">{data.question}</span>
-        <Toggle isActive={isActive} onClick={handleHardToggleClick} />
+        <span className="title-m-semibold">{qnaData.question}</span>
+        <HardQuestionToggle id={qnaData.questionId} />
       </div>
       <Border />
-      <div>{data.answer}</div>
+      <div>{qnaData.answer}</div>
     </div>
   )
 }
 
-const Toggle = ({ isActive, onClick }: { isActive: boolean; onClick: () => void }) => {
-  const isTheme = isActive ? 'gray-white-outline' : 'red-50'
+type toggleType = {
+  id: number
+}
+const HardQuestionToggle = ({ id }: toggleType) => {
+  const [isActive, setIsActive] = useState(false)
+
+  const handleHardToggleClick = () => {
+    setIsActive((prev) => !prev)
+    console.log('이 질문 어려웠어요!', id)
+  }
+
+  const theme = isActive ? 'gray-white-outline' : 'red-50'
   return (
-    <Badge
-      type="hard-label"
-      theme={isTheme}
-      content="이 질문 어려웠어요!"
-      onClick={onClick}
-      className="cursor-pointer"
-    />
+    <button type="button" onClick={handleHardToggleClick} className="cursor-pointer">
+      <Badge type="hard-label" theme={theme} content="이 질문 어려웠어요!" />
+    </button>
   )
 }
