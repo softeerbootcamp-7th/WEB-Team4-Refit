a3143279fffbbd2c8359da16140a0f3860d39d42
zxc534 <zxc534@naver.com>
2026-01-31 15:32:50 +0900

    feat: 특정 면접의 모든 질답 세트 조회 컨트롤러 작성

--- backend/src/main/java/com/shyashyashya/refit/domain/interview/api/InterviewController.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/interview/api/InterviewController.java
@@ -1,19 +1,24 @@
 package com.shyashyashya.refit.domain.interview.api;
 
+import static com.shyashyashya.refit.domain.common.model.ResponseCode.COMMON200;
 import static com.shyashyashya.refit.domain.common.model.ResponseCode.COMMON201;
 import static com.shyashyashya.refit.domain.common.model.ResponseCode.COMMON204;
 
 import com.shyashyashya.refit.domain.common.dto.CommonResponse;
 import com.shyashyashya.refit.domain.interview.dto.request.InterviewCreateRequest;
+import com.shyashyashya.refit.domain.interview.model.QnaSet;
 import com.shyashyashya.refit.domain.interview.service.InterviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,6 +38,12 @@ public class InterviewController {
     public ResponseEntity<CommonResponse<Void>> createInterview(@RequestBody InterviewCreateRequest request) {
         interviewService.createInterview(request);
         var response = CommonResponse.success(COMMON201);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{interviewId}/qna-sets")
+    public ResponseEntity<CommonResponse<>> getQnaSetList(@PathVariable Long interviewId) {
+        var response = CommonResponse.success(COMMON200);
         return ResponseEntity.ok(response);
     }
 }
