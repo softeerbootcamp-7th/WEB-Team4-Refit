5fe249ac5de2da6c179ca4c48f9644a60ba1d798
kckc0608 <kckc0608@naver.com>
2026-02-02 10:00:45 +0900

    feat: dto 클래스 작성

---
+++ backend/src/main/java/com/shyashyashya/refit/domain/interview/dto/request/InterviewSearchRequest.java
@@ -0,0 +1,19 @@
+package com.shyashyashya.refit.domain.interview.dto.request;
+
+import com.shyashyashya.refit.domain.interview.model.InterviewResultStatus;
+import com.shyashyashya.refit.domain.interview.model.InterviewType;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record InterviewSearchRequest(
+        String keyword,
+        InterviewSearchFilter searchFilter
+) {
+    public record InterviewSearchFilter(
+            List<InterviewType> interviewType,
+            List<InterviewResultStatus> interviewResultStatus,
+            LocalDate startDate,
+            LocalDate endDate
+    ) {}
+}
