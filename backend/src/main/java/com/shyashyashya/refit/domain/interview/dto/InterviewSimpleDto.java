ebc65e8c2fc99ac29c38da4b0fb8859948964633
kckc0608 <kckc0608@naver.com>
2026-02-02 15:52:53 +0900

    feat: InterviewSimpleDto 필드를 임시저장 항목 조회 기준으로 단순화

--- backend/src/main/java/com/shyashyashya/refit/domain/interview/dto/InterviewSimpleDto.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/interview/dto/InterviewSimpleDto.java
@@ -11,10 +11,7 @@ public record InterviewSimpleDto(
         Long interviewId,
         InterviewType interviewType,
         LocalDateTime interviewStartAt,
-        InterviewReviewStatus interviewReviewStatus, // TODO : 필드 삭제 논의
-        InterviewResultStatus interviewResultStatus,
         CompanyDto companyInfo,
-        Long jobCategoryId,
         String jobCategoryName,
         LocalDateTime updatedAt) {
     public static InterviewSimpleDto from(Interview interview) {
@@ -22,10 +19,7 @@ public record InterviewSimpleDto(
                 interview.getId(),
                 interview.getInterviewType(),
                 interview.getStartAt(),
-                interview.getReviewStatus(),
-                interview.getResultStatus(),
                 CompanyDto.from(interview.getCompany()),
-                interview.getJobCategory().getId(),
                 interview.getJobCategory().getName(),
                 interview.getUpdatedAt());
     }
