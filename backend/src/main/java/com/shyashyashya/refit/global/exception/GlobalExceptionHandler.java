6c7d848ee803696bb75b1c7cd0bb4552eb8c23f2
lja3723 <lja3723@gmail.com>
2026-02-03 02:14:09 +0900

    feat: LOGIN_REQUIRED 예외 발생시 쿠키도 지우도록 GlobalExceptionHandler 수정

--- backend/src/main/java/com/shyashyashya/refit/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/shyashyashya/refit/global/exception/GlobalExceptionHandler.java
@@ -1,20 +1,33 @@
 package com.shyashyashya.refit.global.exception;
 
+import static com.shyashyashya.refit.global.exception.ErrorCode.LOGIN_REQUIRED;
+
 import com.shyashyashya.refit.domain.common.dto.CommonResponse;
+import com.shyashyashya.refit.global.auth.service.CookieUtil;
+import com.shyashyashya.refit.global.constant.AuthConstant;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@RequiredArgsConstructor
 @Slf4j
 public class GlobalExceptionHandler {
+
+    private final CookieUtil cookieUtil;
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<CommonResponse<Void>> handleCustomException(CustomException e) {
         ErrorCode errorCode = e.getErrorCode();
         log.info("CustomException [{}]: {}", errorCode.name(), errorCode.getMessage());
+
         var response = CommonResponse.customException(errorCode);
+        if (errorCode == LOGIN_REQUIRED) {
+            return handleLoginRequired(response);
+        }
         return ResponseEntity.status(errorCode.getHttpStatus()).body(response);
     }
 
@@ -23,5 +36,16 @@ public class GlobalExceptionHandler {
         log.error("Exception: {}", e.getMessage());
         var response = CommonResponse.unknownException(e);
         return ResponseEntity.internalServerError().body(response);
+    }
+
+    // 로그인 필요한 경우 쿠키를 제거
+    private ResponseEntity<CommonResponse<Void>> handleLoginRequired(CommonResponse<Void> response) {
+        String deleteAccessTokenCookie = cookieUtil.deleteCookie(AuthConstant.ACCESS_TOKEN);
+        String deleteRefreshTokenCookie = cookieUtil.deleteCookie(AuthConstant.REFRESH_TOKEN);
+
+        return ResponseEntity.status(LOGIN_REQUIRED.getHttpStatus())
+                .header(HttpHeaders.SET_COOKIE, deleteAccessTokenCookie)
+                .header(HttpHeaders.SET_COOKIE, deleteRefreshTokenCookie)
+                .body(response);
     }
 }
