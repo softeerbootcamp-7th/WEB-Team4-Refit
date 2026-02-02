79c60aa08b488eb8656dffe7a52717571ecd8a9e
zxc534 <zxc534@naver.com>
2026-02-02 15:20:19 +0900

    feat: 하드코드 값 상수 클래스로 분리

--- backend/src/main/java/com/shyashyashya/refit/domain/interview/dto/request/RawTextUpdateRequest.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/interview/dto/request/RawTextUpdateRequest.java
@@ -1,7 +1,9 @@
 package com.shyashyashya.refit.domain.interview.dto.request;
 
+import static com.shyashyashya.refit.domain.interview.constant.InterviewConstant.RAW_TEXT_MAX_LENGTH;
+
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record RawTextUpdateRequest(
-        @NotNull @Size(max = 10000) String rawText) {}
+        @NotNull @Size(max = RAW_TEXT_MAX_LENGTH) String rawText) {}
