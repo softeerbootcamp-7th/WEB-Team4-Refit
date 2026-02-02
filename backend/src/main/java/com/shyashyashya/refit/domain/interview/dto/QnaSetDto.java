cb1655b034fb1a5acb0a8f91be7aab2c42005a45
zxc534 <zxc534@naver.com>
2026-02-02 16:50:46 +0900

    chore: 불필요한 어노테이션 제거

--- backend/src/main/java/com/shyashyashya/refit/domain/interview/dto/QnaSetDto.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/interview/dto/QnaSetDto.java
@@ -1,20 +1,16 @@
 package com.shyashyashya.refit.domain.interview.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.shyashyashya.refit.domain.qnaset.model.QnaSet;
 import com.shyashyashya.refit.domain.qnaset.model.QnaSetSelfReview;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public record QnaSetDto(
-        @NotNull Long qnaSetId,
-        @NotNull Long interviewId,
-        @NotNull @Size(max = 200) String questionText,
-        @NotNull String answerText,
-        @NotNull String qnaSetSelfReviewText,
+        Long qnaSetId,
+        Long interviewId,
+        String questionText,
+        String answerText,
+        String qnaSetSelfReviewText,
         StarAnalysisDto starAnalysis,
-        @NotNull Boolean isMarkedDifficult) {
+        Boolean isMarkedDifficult) {
     public static QnaSetDto from(QnaSet qnaSet, QnaSetSelfReview qnaSetSelfReview, StarAnalysisDto starAnalysisDto) {
         return new QnaSetDto(
                 qnaSet.getId(),
