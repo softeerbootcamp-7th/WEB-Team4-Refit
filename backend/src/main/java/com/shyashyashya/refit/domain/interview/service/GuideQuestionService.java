2073c4c626729fb1df1091067c4cbda63b7ceccd
zxc534 <zxc534@naver.com>
2026-02-02 16:01:55 +0900

    refactor: 가이드 질문 생성 함수형 인터페이스를 빈으로 변경, 클래스 분리

--- backend/src/main/java/com/shyashyashya/refit/domain/interview/service/GuideQuestionService.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/interview/service/GuideQuestionService.java
@@ -4,11 +4,11 @@ import static com.shyashyashya.refit.global.exception.ErrorCode.INTERVIEW_NOT_FO
 
 import com.shyashyashya.refit.domain.interview.model.Interview;
 import com.shyashyashya.refit.domain.interview.repository.InterviewRepository;
+import com.shyashyashya.refit.domain.interview.service.policy.InterviewGuideQuestionPolicy;
 import com.shyashyashya.refit.domain.interview.service.validator.InterviewValidator;
 import com.shyashyashya.refit.domain.user.model.User;
 import com.shyashyashya.refit.global.exception.CustomException;
 import com.shyashyashya.refit.global.util.RequestUserContext;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +22,8 @@ public class GuideQuestionService {
     private final InterviewValidator interviewValidator;
     private final RequestUserContext requestUserContext;
 
+    private final InterviewGuideQuestionPolicy interviewGuideQuestionPolicy;
+
     @Transactional(readOnly = true)
     public String getGuideQuestion(Long interviewId) {
         User currentUser = requestUserContext.getRequestUser();
@@ -32,28 +34,6 @@ public class GuideQuestionService {
 
         String rawText = interview.getRawText();
 
-        return randomGuideQuestion.generate(rawText);
+        return interviewGuideQuestionPolicy.generate(rawText);
     }
-
-    @FunctionalInterface
-    private interface GuideQuestionPolicy {
-        String generate(String rawText);
-    }
-
-    private final GuideQuestionPolicy randomGuideQuestion = rawText -> {
-        int idx = (int) System.currentTimeMillis() % (GUIDE_QUESTIONS.size());
-        return GUIDE_QUESTIONS.get(idx);
-    };
-
-    private static final List<String> GUIDE_QUESTIONS = List.of(
-            "입사 후 포부가 무엇입니까?",
-            "입사 5년 후, 10년 후 자신의 모습은 어떨 것이라고 생각합니까?",
-            "본인의 직업관은 무엇입니까?",
-            "당신에게 일이 왜 중요합니까?",
-            "직장은 어떤 면을 보고 선택합니까?",
-            "일하는 목적이 무엇입니까?",
-            "어떤 회사가 훌륭한 회사라고 생각합니까?",
-            "인생에서 가장 필요한 사항은 무엇이라 생각하나요?",
-            "많은 직무 중 이 직무를 선택한 이유는 무엇입니까?",
-            "바람직한 사원상은 무엇이라고 생각합니까?");
 }
