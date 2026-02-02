c1d1c4e94cad640e15e0929408ee59b9dfa6fffc
kckc0608 <kckc0608@naver.com>
2026-02-02 09:39:57 +0900

    feat: dto 클래스 작성

---
+++ backend/src/main/java/com/shyashyashya/refit/domain/qnaset/dto/request/QnaSetSearchRequest.java
@@ -0,0 +1,18 @@
+package com.shyashyashya.refit.domain.qnaset.dto.request;
+
+import com.shyashyashya.refit.domain.qnaset.model.StarInclusionLevel;
+
+import java.util.List;
+
+public record QnaSetSearchRequest(
+        String keyword,
+        QnaSearchFilter searchFilter
+) {
+    public record QnaSearchFilter(
+        Boolean hasStarAnalysis,
+        List<StarInclusionLevel> sInclusionLevels,
+        List<StarInclusionLevel> tInclusionLevels,
+        List<StarInclusionLevel> aInclusionLevels,
+        List<StarInclusionLevel> rInclusionLevels
+    ) {}
+}
