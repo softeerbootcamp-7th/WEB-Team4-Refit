e80ec2238e7a1b080cc293441b61ea55d07cc27e
HIHJH <2170095@ewhain.net>
2026-02-02 11:41:42 +0900

    fix: 모바일 최대 사이즈 480px으로 변경

--- frontend/src/routes/layouts/MobileLayout.tsx
+++ frontend/src/routes/layouts/MobileLayout.tsx
@@ -3,10 +3,8 @@ import MobileNavbar from '@/shared/MobileNavbar'
 
 export default function MobileLayout() {
   return (
-    <div className="bg-gray-100 flex min-h-dvh flex-col items-center">
-      <div
-        className="bg-gray-white flex w-full min-h-dvh max-w-[430px] flex-col shadow-[0_0_0_1px_rgba(0,0,0,0.06)]"
-      >
+    <div className="flex min-h-dvh flex-col items-center bg-gray-100">
+      <div className="bg-gray-white flex min-h-dvh w-full max-w-120 flex-col shadow-[0_0_0_1px_rgba(0,0,0,0.06)]">
         <MobileNavbar />
         <main className="flex min-h-0 flex-1 flex-col overflow-auto pt-15">
           <Outlet />
--- frontend/src/shared/MobileNavbar/index.tsx
+++ frontend/src/shared/MobileNavbar/index.tsx
@@ -3,9 +3,7 @@ import { Logo } from '@/assets'
 export default function MobileNavbar() {
   return (
     <nav className="fixed top-0 right-0 left-0 z-50 flex h-15 items-center justify-center">
-      <div
-        className="border-gray-150 bg-gray-white mx-auto flex h-full w-full max-w-[430px] items-center border-b px-6 py-3"
-      >
+      <div className="border-gray-150 bg-gray-white mx-auto flex h-full w-full max-w-120 items-center border-b px-6 py-3">
         <Logo className="h-6 w-auto" />
       </div>
     </nav>
