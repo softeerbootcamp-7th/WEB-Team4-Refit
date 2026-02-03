432d299439d0f0baf49bb8ade8cc32ed6499cc5b
kckc0608 <kckc0608@naver.com>
2026-02-03 15:01:02 +0900

    feat: dto 생성 로직 수정

--- backend/src/main/java/com/shyashyashya/refit/domain/interview/dto/response/DashboardCalendarResponse.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/interview/dto/response/DashboardCalendarResponse.java
@@ -2,13 +2,13 @@ package com.shyashyashya.refit.domain.interview.dto.response;
 
 import com.shyashyashya.refit.domain.interview.dto.InterviewDto;
 import com.shyashyashya.refit.domain.interview.model.Interview;
-
 import java.time.LocalDate;
 import java.util.List;
 
 public record DashboardCalendarResponse(LocalDate date, List<InterviewDto> interviews) {
 
     public static DashboardCalendarResponse of(LocalDate date, List<Interview> interviews) {
-        return new DashboardCalendarResponse(date, interviews.stream().map(InterviewDto::from).toList());
+        return new DashboardCalendarResponse(
+                date, interviews.stream().map(InterviewDto::from).toList());
     }
 }
