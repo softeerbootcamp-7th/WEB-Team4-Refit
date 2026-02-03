0a193003d97131072c474a012673cd7ad13a91c6
kckc0608 <kckc0608@naver.com>
2026-02-03 09:20:55 +0900

    fix: dto 필드명 컨벤션에 맞춰 수정

--- backend/src/main/java/com/shyashyashya/refit/domain/qnaset/dto/QnaSetSimpleDto.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/qnaset/dto/QnaSetSimpleDto.java
@@ -2,7 +2,7 @@ package com.shyashyashya.refit.domain.qnaset.dto;
 
 import com.shyashyashya.refit.domain.qnaset.model.QnaSet;
 
-public record QnaSetSimpleDto(Long qnaSetID, String questionText, String answerText) {
+public record QnaSetSimpleDto(Long qnaSetId, String questionText, String answerText) {
     public static QnaSetSimpleDto from(QnaSet qnaSet) {
         return new QnaSetSimpleDto(qnaSet.getId(), qnaSet.getQuestionText(), qnaSet.getAnswerText());
     }
