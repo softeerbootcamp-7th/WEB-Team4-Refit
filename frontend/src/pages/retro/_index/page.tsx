35be1073a696655b10ae45373299fda28f60b445
HIHJH <2170095@ewhain.net>
2026-02-04 06:02:15 +0900

    feat: 회고 sidebar 컴포넌트 구현

--- frontend/src/pages/retro/_index/page.tsx
+++ frontend/src/pages/retro/_index/page.tsx
@@ -1,6 +1,6 @@
 // import { RetroPdfPanel } from './components/pdf-panel/RetroPdfPanel'
 // import { RetroSection } from './components/retro-section/RetroSection'
-// import { RetroMinimizedSidebar, RetroSidebar } from './components/sidebar'
+import { RetroMinimizedSidebar, RetroSidebar } from './components/sidebar'
 import { RetroProvider, useRetroContext } from './contexts'
 
 export default function RetroQuestionPage() {
@@ -17,15 +17,15 @@ function RetroLayout() {
   if (!isPdfOpen) {
     return (
       <div className="grid h-full grid-cols-[320px_1fr]">
-        {/* <RetroSidebar />
-        <RetroSection /> */}
+        <RetroSidebar />
+        {/* <RetroSection /> */}
       </div>
     )
   }
   return (
     <div className="grid h-full grid-cols-[80px_1fr_1fr]">
-      {/* <RetroMinimizedSidebar />
-      <RetroSection />
+      <RetroMinimizedSidebar />
+      {/* <RetroSection />
       <RetroPdfPanel /> */}
     </div>
   )
