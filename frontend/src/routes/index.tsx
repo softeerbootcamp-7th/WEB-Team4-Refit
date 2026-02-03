d07da2a70557c1b55a7f94eb61eed30df8d54d6b
HIHJH <2170095@ewhain.net>
2026-02-04 06:00:20 +0900

    feat: 면접 회고 페이지 라우팅 연결

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -4,6 +4,7 @@ import {
   DashboardPage,
   RecordConfirmPage,
   RecordLinkPage,
+  RetroQuestionPage,
   NotFound,
   SharedComponentExample,
   SigninPage,
@@ -87,7 +88,7 @@ const router = createBrowserRouter([
         children: [
           {
             path: getChildPath(ROUTES.RETRO_QUESTION, ROUTES.RETRO),
-            Component: DashboardPage,
+            Component: RetroQuestionPage,
           },
           {
             path: getChildPath(ROUTES.RETRO_DETAILS, ROUTES.RETRO),
