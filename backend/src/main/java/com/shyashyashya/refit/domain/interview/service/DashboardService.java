40d345fe687e89662f6a8371ebd481c14c3be6c0
Everdu <kckc0608@naver.com>
2026-02-04 14:37:49 +0900

    [DEV-75/BE] feat: 곧 있을 면접 정보 조회 API (#195)
    * refactor: RequestUserContext 전역 Getter 제거 (#179)
    
    * feat: 대시보드 헤드라인 데이터 조회 API 작성
    
    * chore: spotless 적용
    
    * refactor: 레포지토리 메서드 네이밍 수정
    
    * fix: 쿼리문 오타 수정
    
    * refactor: 특정 리뷰 상태의 인터뷰를 조회하는 로직을 쿼리메서드 방식으로 개선
    
    * refaactor: review status 리스트를 매번 생성하지 않도록 상수화
    
    * feat: Dto 클래스 작성
    
    * feat: repository 쿼리 작성
    
    * feat: API 로직 작성
    
    * refator: Pageable 적용
    
    * chore: 레포지토리 메서드 명 수정
    
    * fix: Page 응답에 맞춰 stream 코드 수정
    
    * fix: stream() 코드 오타 수정
    
    * fix: api path 오타수정
    
    * chore: spotless 적용
    
    ---------
    
    Co-authored-by: Jangan Lee <lja3723@gmail.com>

--- backend/src/main/java/com/shyashyashya/refit/domain/interview/service/DashboardService.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/interview/service/DashboardService.java
@@ -5,15 +5,21 @@ import static com.shyashyashya.refit.domain.interview.model.InterviewReviewStatu
 import static com.shyashyashya.refit.domain.interview.model.InterviewReviewStatus.SELF_REVIEW_DRAFT;
 
 import com.shyashyashya.refit.domain.interview.dto.response.DashboardHeadlineResponse;
+import com.shyashyashya.refit.domain.interview.dto.response.DashboardUpcomingInterviewResponse;
 import com.shyashyashya.refit.domain.interview.model.Interview;
 import com.shyashyashya.refit.domain.interview.model.InterviewReviewStatus;
 import com.shyashyashya.refit.domain.interview.repository.InterviewRepository;
+import com.shyashyashya.refit.domain.qnaset.model.QnaSet;
+import com.shyashyashya.refit.domain.qnaset.repository.QnaSetRepository;
 import com.shyashyashya.refit.domain.user.model.User;
 import com.shyashyashya.refit.global.util.RequestUserContext;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,6 +32,7 @@ public class DashboardService {
 
     private final RequestUserContext requestUserContext;
     private final InterviewRepository interviewRepository;
+    private final QnaSetRepository qnaSetRepository;
 
     @Transactional(readOnly = true)
     public DashboardHeadlineResponse getDashboardHeadlineData() {
@@ -35,9 +42,10 @@ public class DashboardService {
             return DashboardHeadlineResponse.registerInterview(requestUser);
         }
 
+        Pageable pageable = PageRequest.of(0, 1);
         LocalDateTime now = LocalDateTime.now();
-        return interviewRepository
-                .getUpcomingInterview(requestUser, now, now.plusDays(7))
+        return interviewRepository.getUpcomingInterview(requestUser, now, now.plusDays(7), pageable).stream()
+                .findFirst()
                 .map(interview -> {
                     long dDay = getInterviewDday(now, interview);
                     return DashboardHeadlineResponse.prepareInterview(requestUser, dDay);
@@ -47,6 +55,23 @@ public class DashboardService {
                         return DashboardHeadlineResponse.reviewInterview(requestUser);
                     }
                     return DashboardHeadlineResponse.checkInterviewHistory(requestUser);
+                });
+    }
+
+    @Transactional(readOnly = true)
+    public Page<DashboardUpcomingInterviewResponse> getUpcomingInterviews(Pageable pageable) {
+        User requestUser = requestUserContext.getRequestUser();
+
+        // TODO : 단일 쿼리로 조회해오도록 로직 수정
+        LocalDateTime now = LocalDateTime.now();
+        return interviewRepository
+                .getUpcomingInterview(requestUser, now, now.plusDays(7), pageable)
+                .map(interview -> {
+                    List<QnaSet> similarTrendQuestions = qnaSetRepository.findAllByIndustryAndJobCategory(
+                            interview.getIndustry(), interview.getJobCategory());
+                    List<Interview> similarInterviews = interviewRepository.findAllSimilarInterviewsByUser(
+                            requestUser, interview.getIndustry(), interview.getJobCategory());
+                    return DashboardUpcomingInterviewResponse.of(interview, similarTrendQuestions, similarInterviews);
                 });
     }
 
