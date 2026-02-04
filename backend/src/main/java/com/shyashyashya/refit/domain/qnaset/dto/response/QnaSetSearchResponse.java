e21d4fcf3936f86594c776166bf9028f5584673f
lja3723 <lja3723@gmail.com>
2026-02-04 18:04:01 +0900

    refactor: QnaSetSearchResponseDto 스펙 변경

--- backend/src/main/java/com/shyashyashya/refit/domain/qnaset/dto/response/QnaSetSearchResponse.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/qnaset/dto/response/QnaSetSearchResponse.java
@@ -1,11 +1,15 @@
 package com.shyashyashya.refit.domain.qnaset.dto.response;
 
-import com.shyashyashya.refit.domain.interview.dto.InterviewSimpleDto;
+import com.shyashyashya.refit.domain.interview.dto.InterviewDto;
 import com.shyashyashya.refit.domain.qnaset.dto.QnaSetSimpleDto;
 import com.shyashyashya.refit.domain.qnaset.model.QnaSet;
+import lombok.AccessLevel;
+import lombok.Builder;
 
-public record QnaSetSearchResponse(InterviewSimpleDto interviewInfo, QnaSetSimpleDto qnaSetSimpleDto) {
+@Builder(access = AccessLevel.PRIVATE)
+public record QnaSetSearchResponse(
+    InterviewDto interviewInfo, QnaSetSimpleDto qnaSetInfo) {
     public static QnaSetSearchResponse from(QnaSet qnaSet) {
-        return new QnaSetSearchResponse(InterviewSimpleDto.from(qnaSet.getInterview()), QnaSetSimpleDto.from(qnaSet));
+        return new QnaSetSearchResponse(InterviewDto.from(qnaSet.getInterview()), QnaSetSimpleDto.from(qnaSet));
     }
 }
