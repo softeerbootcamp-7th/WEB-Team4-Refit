abf35c7247bd3481cfdffca2f0cfb8f1f88075de
lja3723 <lja3723@gmail.com>
2026-02-04 19:03:50 +0900

    feat: 스크랩 폴더의 소유권을 검증하는 검증기 추가

---
+++ backend/src/main/java/com/shyashyashya/refit/domain/scrapfolder/service/validator/ScrapFolderValidator.java
@@ -0,0 +1,19 @@
+package com.shyashyashya.refit.domain.scrapfolder.service.validator;
+
+import static com.shyashyashya.refit.global.exception.ErrorCode.SCRAP_FOLDER_NOT_ACCESSIBLE;
+
+import com.shyashyashya.refit.domain.scrapfolder.model.ScrapFolder;
+import com.shyashyashya.refit.domain.user.model.User;
+import com.shyashyashya.refit.global.exception.CustomException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScrapFolderValidator {
+
+    public void validateScrapFolderOwner(ScrapFolder scrapFolder, User user) {
+        if (scrapFolder.getUser().equals(user)) {
+            return;
+        }
+        throw new CustomException(SCRAP_FOLDER_NOT_ACCESSIBLE);
+    }
+}
