688885ba5e4bcb33cd92894caf2e8fea3ef4f7cd
lja3723 <lja3723@gmail.com>
2026-02-04 19:06:11 +0900

    refactor: 정팩메 파라미터 단순화

--- backend/src/main/java/com/shyashyashya/refit/domain/scrapfolder/dto/response/ScrapFolderQnaSetResponse.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/scrapfolder/dto/response/ScrapFolderQnaSetResponse.java
@@ -1,7 +1,6 @@
 package com.shyashyashya.refit.domain.scrapfolder.dto.response;
 
 import com.shyashyashya.refit.domain.interview.dto.InterviewDto;
-import com.shyashyashya.refit.domain.interview.model.Interview;
 import com.shyashyashya.refit.domain.qnaset.dto.QnaSetSimpleDto;
 import com.shyashyashya.refit.domain.qnaset.model.QnaSet;
 import lombok.AccessLevel;
@@ -10,10 +9,10 @@ import lombok.Builder;
 @Builder(access = AccessLevel.PRIVATE)
 public record ScrapFolderQnaSetResponse(InterviewDto interview, QnaSetSimpleDto qnaSet) {
 
-    public static ScrapFolderQnaSetResponse from(QnaSet qnaSet, Interview interview) {
+    public static ScrapFolderQnaSetResponse from(QnaSet qnaSet) {
         return ScrapFolderQnaSetResponse.builder()
-            .interview(InterviewDto.from(interview))
-            .qnaSet(QnaSetSimpleDto.from(qnaSet))
-            .build();
+                .interview(InterviewDto.from(qnaSet.getInterview()))
+                .qnaSet(QnaSetSimpleDto.from(qnaSet))
+                .build();
     }
 }
