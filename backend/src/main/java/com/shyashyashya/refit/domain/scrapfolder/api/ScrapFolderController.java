838170ec0c8140d4a18ec01b5ea8fc48bf332c85
lja3723 <lja3723@gmail.com>
2026-02-04 19:04:55 +0900

    feat: 스크랩 폴더의 질답 세트 목록 조회하는 API 추가

--- backend/src/main/java/com/shyashyashya/refit/domain/scrapfolder/api/ScrapFolderController.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/scrapfolder/api/ScrapFolderController.java
@@ -3,6 +3,7 @@ package com.shyashyashya.refit.domain.scrapfolder.api;
 import static com.shyashyashya.refit.domain.common.model.ResponseCode.COMMON200;
 
 import com.shyashyashya.refit.domain.common.dto.CommonResponse;
+import com.shyashyashya.refit.domain.scrapfolder.dto.response.ScrapFolderQnaSetResponse;
 import com.shyashyashya.refit.domain.scrapfolder.dto.response.ScrapFolderResponse;
 import com.shyashyashya.refit.domain.scrapfolder.service.ScrapFolderService;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +25,14 @@ public class ScrapFolderController {
     @GetMapping
     public ResponseEntity<CommonResponse<Page<ScrapFolderResponse>>> getMyScrapFolders(Pageable pageable) {
         var body = scrapFolderService.getMyScrapFolders(pageable);
+        var response = CommonResponse.success(COMMON200, body);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{scrapFolderId}")
+    public ResponseEntity<CommonResponse<Page<ScrapFolderQnaSetResponse>>> getQnaSetsInScrapFolder(
+            @PathVariable Long scrapFolderId, Pageable pageable) {
+        var body = scrapFolderService.getQnaSetsInScrapFolder(scrapFolderId, pageable);
         var response = CommonResponse.success(COMMON200, body);
         return ResponseEntity.ok(response);
     }
