e95ab4d3be07d99f9fca7236a3280d5ce7eb14d5
Jangan Lee <lja3723@gmail.com>
2026-02-05 19:20:09 +0900

    [DEV-98/BE] feat: 스크랩 폴더 조회 API 개발 (#218)
    * rename: 패키지 이름을 scrapFoler로 설정
    
    * feat: 사용자의 스크랩폴더를 페이지로 반환하는 레포지토리 추가
    
    * feat: 스크랩 폴더의 질답 세트 개수를 세는 레포지토리 추가
    
    * feat: 스크랩 폴더 정보를 표현하는 DTO 추가
    
    * feat: 스크랩 폴더 서비스 추가
    
    * feat: 스크랩 폴더 컨트롤러 추가
    
    * chore: spotless 적용

---
+++ backend/src/main/java/com/shyashyashya/refit/domain/scrapfolder/api/ScrapFolderController.java
@@ -0,0 +1,29 @@
+package com.shyashyashya.refit.domain.scrapfolder.api;
+
+import static com.shyashyashya.refit.domain.common.model.ResponseCode.COMMON200;
+
+import com.shyashyashya.refit.domain.common.dto.CommonResponse;
+import com.shyashyashya.refit.domain.scrapfolder.dto.ScrapFolderDto;
+import com.shyashyashya.refit.domain.scrapfolder.service.ScrapFolderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/scrap-folder")
+@RequiredArgsConstructor
+public class ScrapFolderController {
+
+    private final ScrapFolderService scrapFolderService;
+
+    @GetMapping
+    public ResponseEntity<CommonResponse<Page<ScrapFolderDto>>> getMyScrapFolders(Pageable pageable) {
+        var body = scrapFolderService.getMyScrapFolders(pageable);
+        var response = CommonResponse.success(COMMON200, body);
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ backend/src/main/java/com/shyashyashya/refit/domain/scrapfolder/repository/ScrapFolderRepository.java
@@ -0,0 +1,12 @@
+package com.shyashyashya.refit.domain.scrapfolder.repository;
+
+import com.shyashyashya.refit.domain.scrapfolder.model.ScrapFolder;
+import com.shyashyashya.refit.domain.user.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScrapFolderRepository extends JpaRepository<ScrapFolder, Long> {
+
+    Page<ScrapFolder> getScrapFoldersByUser(User user, Pageable pageable);
+}
