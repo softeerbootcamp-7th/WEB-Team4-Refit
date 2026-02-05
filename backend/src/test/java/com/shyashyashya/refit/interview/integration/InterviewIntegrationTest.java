61cadbc38d72d1f8230948b6b41bed5d80d3aa6c
kckc0608 <kckc0608@naver.com>
2026-02-05 19:21:14 +0900

    test: 면접 단일 조회 통합테스트 작성

--- backend/src/test/java/com/shyashyashya/refit/interview/integration/InterviewIntegrationTest.java
+++ backend/src/test/java/com/shyashyashya/refit/interview/integration/InterviewIntegrationTest.java
@@ -2,8 +2,11 @@ package com.shyashyashya.refit.interview.integration;
 
 import static com.shyashyashya.refit.domain.common.model.ResponseCode.COMMON200;
 import static com.shyashyashya.refit.domain.common.model.ResponseCode.COMMON201;
+import static com.shyashyashya.refit.global.exception.ErrorCode.INTERVIEW_NOT_ACCESSIBLE;
+import static com.shyashyashya.refit.global.exception.ErrorCode.INTERVIEW_NOT_FOUND;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 
 import com.shyashyashya.refit.core.IntegrationTest;
@@ -11,6 +14,8 @@ import com.shyashyashya.refit.domain.interview.dto.request.InterviewCreateReques
 import com.shyashyashya.refit.domain.interview.model.InterviewType;
 import java.time.LocalDateTime;
 
+import com.shyashyashya.refit.domain.user.model.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -55,6 +60,66 @@ public class InterviewIntegrationTest extends IntegrationTest {
                     .body("code", equalTo(COMMON201.name()))
                     .body("message", equalTo(COMMON201.getMessage()))
                     .body("result", nullValue());
+        }
+    }
+
+    @Nested
+    class 면접_단일_조회_시 {
+
+        private static final String path = "/interview";
+        private static Long interviewId;
+
+        @BeforeEach
+        void setUp() {
+            InterviewCreateRequest request = new InterviewCreateRequest(
+                    LocalDateTime.of(2025, 12, 29, 10, 0, 0), InterviewType.FIRST, "현대자동차", 1L, 1L, "BE Developer");
+            interviewId = createInterview(request).getId();
+        }
+
+        @Test
+        void 성공한다() {
+            // when & then
+            given(spec).
+            when().
+                    get(path + "/" + interviewId).
+            then().
+                    statusCode(200).
+                    body("code", equalTo(COMMON200.name())).
+                    body("message", equalTo(COMMON200.getMessage())).
+                    body("result", notNullValue()).
+                    body("result.interviewId", notNullValue());
+        }
+
+        @Test
+        void 존재하지_않는_면접을_조회하면_실패한다() {
+            // when & then
+            given(spec).
+                    when().
+                    get(path + "/" + (interviewId + 1)).
+                    then().
+                    statusCode(404).
+                    body("code", equalTo(INTERVIEW_NOT_FOUND.name())).
+                    body("message", equalTo(INTERVIEW_NOT_FOUND.getMessage())).
+                    body("result", nullValue());
+        }
+
+        @Test
+        void 로그인한_사용자가_아닌_다른_사람의_면접을_조회하면_실패한다() {
+            // given
+            InterviewCreateRequest request = new InterviewCreateRequest(
+                    LocalDateTime.of(2025, 12, 29, 10, 0, 0), InterviewType.FIRST, "현대자동차", 1L, 1L, "BE Developer");
+            User user = createUser("other@example.com", "other", industry, jobCategory);
+            Long otherInterviewId = createInterview(request, user).getId();
+
+            // when & then
+            given(spec).
+            when().
+                    get(path + "/" + otherInterviewId).
+            then().
+                    statusCode(403).
+                    body("code", equalTo(INTERVIEW_NOT_ACCESSIBLE.name())).
+                    body("message", equalTo(INTERVIEW_NOT_ACCESSIBLE.getMessage())).
+                    body("result", nullValue());
         }
     }
 }
