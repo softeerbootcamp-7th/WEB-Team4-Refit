53a771fd0f2c0340e3f5f27bfecdbe929fe49040
lja3723 <lja3723@gmail.com>
2026-02-06 20:50:09 +0900

    feat: Dto 정팩페 작성

--- backend/src/main/java/com/shyashyashya/refit/global/oauth2/dto/OAuthLoginUrlResponse.java
+++ backend/src/main/java/com/shyashyashya/refit/global/oauth2/dto/OAuthLoginUrlResponse.java
@@ -1,3 +1,12 @@
 package com.shyashyashya.refit.global.oauth2.dto;
 
-public record OAuthLoginUrlResponse(String oAuthLoginUrl) {}
+import lombok.AccessLevel;
+import lombok.Builder;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record OAuthLoginUrlResponse(String oAuthLoginUrl) {
+
+    public static OAuthLoginUrlResponse from(String oAuthLoginUrl) {
+        return OAuthLoginUrlResponse.builder().oAuthLoginUrl(oAuthLoginUrl).build();
+    }
+}
