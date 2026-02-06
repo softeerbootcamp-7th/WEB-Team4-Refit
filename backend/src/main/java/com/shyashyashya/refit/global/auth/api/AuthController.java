9000bff79308f7c33013d6cdcb059e34477d5644
kckc0608 <kckc0608@naver.com>
2026-02-06 21:26:49 +0900

    docs: Auth API 에 대한 스웨거 문서 작성

--- backend/src/main/java/com/shyashyashya/refit/global/auth/api/AuthController.java
+++ backend/src/main/java/com/shyashyashya/refit/global/auth/api/AuthController.java
@@ -6,6 +6,8 @@ import com.shyashyashya.refit.domain.common.dto.CommonResponse;
 import com.shyashyashya.refit.global.auth.service.AuthService;
 import com.shyashyashya.refit.global.auth.service.CookieUtil;
 import com.shyashyashya.refit.global.constant.AuthConstant;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Auth Controller", description = "인증 관련 API 입니다.")
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
@@ -22,6 +25,7 @@ public class AuthController {
     private final AuthService authService;
     private final CookieUtil cookieUtil;
 
+    @Operation(summary = "리프레시 토큰과 엑세스 토큰을 재발급합니다.", description = "리프레시 토큰이 재발급되면, 기존 리프레시 토큰은 폐기됩니다.")
     @GetMapping("/reissue")
     public ResponseEntity<CommonResponse<Void>> reissue(
             @CookieValue(value = AuthConstant.ACCESS_TOKEN) String accessToken,
