81b221f13d78c4ad10a8826826e300b81868d4a0
zxc534 <zxc534@naver.com>
2026-02-06 10:47:00 +0900

    feat: 생성 서비스 로직 뼈대 작성

--- backend/src/main/java/com/shyashyashya/refit/domain/qnaset/service/StarAnalysisService.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/qnaset/service/StarAnalysisService.java
@@ -1,12 +1,14 @@
 package com.shyashyashya.refit.domain.qnaset.service;
 
 import static com.shyashyashya.refit.global.exception.ErrorCode.QNA_SET_NOT_FOUND;
+import static com.shyashyashya.refit.global.exception.ErrorCode.STAR_ANALYSIS_CREATION_ALREADY_IN_PROGRESS;
 
 import com.shyashyashya.refit.domain.interview.dto.StarAnalysisDto;
 import com.shyashyashya.refit.domain.interview.model.Interview;
 import com.shyashyashya.refit.domain.interview.service.validator.InterviewValidator;
 import com.shyashyashya.refit.domain.qnaset.model.QnaSet;
 import com.shyashyashya.refit.domain.qnaset.model.StarAnalysis;
+import com.shyashyashya.refit.domain.qnaset.model.StarAnalysisStatus;
 import com.shyashyashya.refit.domain.qnaset.model.StarInclusionLevel;
 import com.shyashyashya.refit.domain.qnaset.repository.QnaSetRepository;
 import com.shyashyashya.refit.domain.qnaset.repository.StarAnalysisRepository;
@@ -16,6 +18,12 @@ import com.shyashyashya.refit.global.util.RequestUserContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.request.async.DeferredResult;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 @RequiredArgsConstructor
@@ -26,6 +34,9 @@ public class StarAnalysisService {
 
     private final RequestUserContext requestUserContext;
     private final InterviewValidator interviewValidator;
+    private final WebClient webClient;
+
+    private final Map<Long, DeferredResult<StarAnalysisDto>> waiting = new ConcurrentHashMap<>();
 
     @Transactional
     public StarAnalysisDto createStarAnalysis(Long qnaSetId) {
@@ -58,6 +69,61 @@ public class StarAnalysisService {
                 StarInclusionLevel.PRESENT,
                 StarInclusionLevel.INSUFFICIENT,
                 "전체 요약 텍스트 임시 가짜 데이터",
+                StarAnalysisStatus.COMPLETED,
                 qnaSet);
+    }
+
+    public DeferredResult<StarAnalysisDto> createStarAnalysisLongPoll(Long qnaSetId) {
+        QnaSet qnaSet = getValidatedQnaSetForUser(qnaSetId);
+
+        if (starAnalysisRepository.existsByQnaSet(qnaSet)) {
+            throw new CustomException(STAR_ANALYSIS_CREATION_ALREADY_IN_PROGRESS);
+        } else {
+            StarAnalysis starAnalysis = starAnalysisRepository.save(StarAnalysis.createInProgressStarAnalysis(qnaSet));
+
+            // TODO 하드코드 값 상수화
+            Long timeoutMs = Duration.ofSeconds(20).toMillis();
+            DeferredResult<StarAnalysisDto> deferredResult = new DeferredResult<>(timeoutMs);
+
+            // waiting 맵의 용도는 무엇인가?
+            waiting.put(starAnalysis.getId(), deferredResult);
+
+            deferredResult.onTimeout(() -> {
+                // TODO 타임아웃 동작 작성
+            });
+
+            deferredResult.onCompletion(() -> {
+                // TODO 클라이언트 연결 끊긴 경우 동작 작성
+            });
+
+            callGeminiStarAnalysis(qnaSet);
+
+            return deferredResult;
+        }
+    }
+
+    private void callGeminiStarAnalysis(QnaSet qnaSet) {
+        final String GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent";
+        final String GEMINI_API_KEY = System.getenv("GEMINI_API_KEY");
+
+        webClient.post()
+                .uri(GEMINI_API_URL)
+                .header("x-goog-api-key", GEMINI_API_KEY)
+                .header("Content-Type", "application/json")
+                .bodyValue()
+                .retrieve()
+                .bodyToMono()
+                .subscribe(
+                        ok -> onRequestSuccess(),
+                        err -> onRequestFail()
+                );
+    }
+
+    private void onRequestSuccess() {
+
+    }
+
+    private void onRequestFail() {
+
     }
 }
