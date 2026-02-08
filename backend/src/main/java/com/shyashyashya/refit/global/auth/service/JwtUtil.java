377dd77ca59dc0df33a2440cb6c0befe339b0282
lja3723 <lja3723@gmail.com>
2026-02-09 00:36:59 +0900

    refactor: 값 객체 활용하여 리팩터링 수행

--- backend/src/main/java/com/shyashyashya/refit/global/auth/service/JwtUtil.java
+++ backend/src/main/java/com/shyashyashya/refit/global/auth/service/JwtUtil.java
@@ -1,17 +1,20 @@
 package com.shyashyashya.refit.global.auth.service;
 
-import static com.shyashyashya.refit.global.exception.ErrorCode.LOGIN_REQUIRED;
 import static com.shyashyashya.refit.global.exception.ErrorCode.TOKEN_EXPIRED;
+import static com.shyashyashya.refit.global.exception.ErrorCode.TOKEN_REQUIRED;
+import static com.shyashyashya.refit.global.exception.ErrorCode.TOKEN_VALIDATION_FAILED;
 
+import com.shyashyashya.refit.global.auth.model.JwtTokenType;
+import com.shyashyashya.refit.global.auth.model.ValidatedJwtToken;
 import com.shyashyashya.refit.global.exception.CustomException;
 import com.shyashyashya.refit.global.property.AuthJwtProperty;
+import com.shyashyashya.refit.global.property.OAuth2Property;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotNull;
@@ -27,94 +30,124 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class JwtUtil {
 
+    private static final String CLAIM_KEY_JWT_TOKEN_TYPE = "type";
+    private static final String CLAIM_KEY_USER_ID = "userId";
+
     private final Key key;
     private final Duration accessTokenExpiration;
     private final Duration refreshTokenExpiration;
+    private final Duration oAuth2StateTokenExpiration;
     private final JwtParser jwtParser;
 
-    public JwtUtil(AuthJwtProperty authJwtProperty) {
+    public JwtUtil(AuthJwtProperty authJwtProperty, OAuth2Property oAuth2Property) {
         this.key = Keys.hmacShaKeyFor(authJwtProperty.secret().getBytes());
         this.accessTokenExpiration = authJwtProperty.tokenExpiration().accessToken();
         this.refreshTokenExpiration = authJwtProperty.tokenExpiration().refreshToken();
+        this.oAuth2StateTokenExpiration = oAuth2Property.stateExpiration();
         this.jwtParser = Jwts.parserBuilder().setSigningKey(key).build();
     }
 
     public String createAccessToken(@NotNull String email, @Nullable Long userId) {
-        return createJwtToken(email, userId, accessTokenExpiration);
+        return createAccessOrRefreshJwtToken(email, userId, accessTokenExpiration, JwtTokenType.ACCESS_TOKEN);
     }
 
     public String createRefreshToken(@NotNull String email, @Nullable Long userId) {
-        return createJwtToken(email, userId, refreshTokenExpiration);
+        return createAccessOrRefreshJwtToken(email, userId, refreshTokenExpiration, JwtTokenType.REFRESH_TOKEN);
     }
 
-    public String getEmail(String token) {
-        return parseClaims(token).getSubject();
+    public String createOAuth2StateToken(String requestHostUrl) {
+        var now = Instant.now();
+        var expiration = now.plus(oAuth2StateTokenExpiration);
+
+        return Jwts.builder()
+                .setSubject(requestHostUrl)
+                .claim(CLAIM_KEY_JWT_TOKEN_TYPE, JwtTokenType.OAUTH2_STATE_TOKEN)
+                .setIssuedAt(Date.from(now))
+                .setExpiration(Date.from(expiration))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
     }
 
-    public Optional<Long> getUserId(String token) {
-        return Optional.ofNullable(parseClaims(token).get("userId", Long.class));
-    }
-
-    public Instant getExpiration(String token) {
-        return parseClaims(token).getExpiration().toInstant();
-    }
-
-    public boolean isTokenExpired(String token) {
-        Date expiration = parseClaims(token).getExpiration();
-        return expiration.before(new Date());
-    }
-
-    public void validateToken(String token) {
-        try {
-            if (token == null) {
-                throw new CustomException(LOGIN_REQUIRED);
-            } else if (isTokenExpired(token)) {
-                throw new CustomException(TOKEN_EXPIRED);
-            }
-            return;
-        } catch (SecurityException | MalformedJwtException e) {
-            log.warn("Invalid JWT signature: {}", e.getMessage());
-        } catch (UnsupportedJwtException e) {
-            log.warn("Unsupported JWT token: {}", e.getMessage());
-        } catch (IllegalArgumentException e) {
-            log.warn("JWT claims string is empty: {}", e.getMessage());
+    public ValidatedJwtToken getValidatedJwtToken(String token) {
+        var jwtToken = getValidatedJwtTokenAllowExpired(token);
+        if (jwtToken.isExpired()) {
+            throw new CustomException(TOKEN_EXPIRED);
         }
-        throw new CustomException(LOGIN_REQUIRED);
+        return jwtToken;
     }
 
-    public void validateTokenIgnoringExpiration(String token) {
+    public ValidatedJwtToken getValidatedJwtTokenAllowExpired(String token) {
         try {
-            if (token == null) {
-                throw new CustomException(LOGIN_REQUIRED);
+            if (token == null || token.isBlank()) {
+                throw new CustomException(TOKEN_REQUIRED);
             }
-            parseClaims(token);
-            return;
-        } catch (SecurityException | MalformedJwtException | UnsupportedJwtException | IllegalArgumentException e) {
-            log.warn("Invalid JWT token: {}", e.getMessage());
-        }
-        throw new CustomException(LOGIN_REQUIRED);
-    }
 
-    private Claims parseClaims(String token) {
-        try {
-            return jwtParser.parseClaimsJws(token).getBody();
+            Claims claims = jwtParser.parseClaimsJws(token).getBody();
+            String jwtTokenType = claims.get(CLAIM_KEY_JWT_TOKEN_TYPE, String.class);
+            return ValidatedJwtToken.createUnexpiredToken(claims, JwtTokenType.valueOf(jwtTokenType));
+
         } catch (ExpiredJwtException e) {
-            return e.getClaims();
+            Claims claims = e.getClaims();
+            String jwtTokenType = claims.get(CLAIM_KEY_JWT_TOKEN_TYPE, String.class);
+            return ValidatedJwtToken.createExpiredToken(claims, JwtTokenType.valueOf(jwtTokenType));
+
+        } catch (JwtException e) {
+            log.warn("Invalid JWT token: {}", e.getMessage());
+            throw new CustomException(TOKEN_VALIDATION_FAILED);
         }
     }
 
-    private String createJwtToken(@NotNull String email, @Nullable Long userId, @NotNull Duration expirationDuration) {
+    public String getRequestHostUrl(ValidatedJwtToken validatedJwtToken) {
+        validateOAuth2StateType(validatedJwtToken);
+        return validatedJwtToken.claims().getSubject();
+    }
+
+    public String getEmail(ValidatedJwtToken validatedJwtToken) {
+        validateAccessOrRefreshType(validatedJwtToken);
+        return validatedJwtToken.claims().getSubject();
+    }
+
+    public Optional<Long> getUserId(ValidatedJwtToken validatedJwtToken) {
+        validateAccessOrRefreshType(validatedJwtToken);
+        return Optional.ofNullable(validatedJwtToken.claims().get(CLAIM_KEY_USER_ID, Long.class));
+    }
+
+    private void validateAccessOrRefreshType(ValidatedJwtToken jwtToken) {
+        if (jwtToken.type() != JwtTokenType.ACCESS_TOKEN && jwtToken.type() != JwtTokenType.REFRESH_TOKEN) {
+            log.warn("ACCESS_TOKEN or REFRESH_TOKEN type expected.");
+            throw new CustomException(TOKEN_VALIDATION_FAILED);
+        }
+    }
+
+    private void validateOAuth2StateType(ValidatedJwtToken jwtToken) {
+        if (jwtToken.type() != JwtTokenType.OAUTH2_STATE_TOKEN) {
+            log.warn("OAUTH2_STATE_TOKEN type expected.");
+            throw new CustomException(TOKEN_VALIDATION_FAILED);
+        }
+    }
+
+    private String createAccessOrRefreshJwtToken(
+            @NotNull String email,
+            @Nullable Long userId,
+            @NotNull Duration expirationDuration,
+            @NotNull JwtTokenType jwtTokenType) {
+
+        if (jwtTokenType == JwtTokenType.OAUTH2_STATE_TOKEN) {
+            throw new IllegalArgumentException("ACCESS_TOKEN or REFRESH_TOKEN type expected.");
+        }
+
         var now = Instant.now();
         var expiration = now.plus(expirationDuration);
 
         var builder = Jwts.builder()
                 .setSubject(email)
+                .claim(CLAIM_KEY_JWT_TOKEN_TYPE, jwtTokenType)
                 .setIssuedAt(Date.from(now))
                 .setExpiration(Date.from(expiration))
                 .signWith(key, SignatureAlgorithm.HS256);
 
         if (userId != null) {
-            builder.claim("userId", userId);
+            builder.claim(CLAIM_KEY_USER_ID, userId);
         }
 
         return builder.compact();
