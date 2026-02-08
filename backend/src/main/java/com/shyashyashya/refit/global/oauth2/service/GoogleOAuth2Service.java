8d8ee0937cccb27ee4cd59e9598be65a1ec5147b
lja3723 <lja3723@gmail.com>
2026-02-09 01:22:03 +0900

    refactor: EnvironmentType 대신 JWT로 frontendRedirectUri 구성

--- backend/src/main/java/com/shyashyashya/refit/global/oauth2/service/GoogleOAuth2Service.java
+++ backend/src/main/java/com/shyashyashya/refit/global/oauth2/service/GoogleOAuth2Service.java
@@ -3,23 +3,20 @@ package com.shyashyashya.refit.global.oauth2.service;
 import static com.shyashyashya.refit.global.exception.ErrorCode.EXTERNAL_OAUTH_SERVER_ERROR;
 import static com.shyashyashya.refit.global.exception.ErrorCode.INVALID_OAUTH_ACCESS_TOKEN;
 import static com.shyashyashya.refit.global.exception.ErrorCode.INVALID_OAUTH_CODE;
+import static com.shyashyashya.refit.global.exception.ErrorCode.REQUEST_HOST_OAUTH_REDIRECTION_NOT_ALLOWED;
 
 import com.shyashyashya.refit.domain.user.model.User;
 import com.shyashyashya.refit.domain.user.repository.UserRepository;
 import com.shyashyashya.refit.global.auth.model.RefreshToken;
 import com.shyashyashya.refit.global.auth.repository.RefreshTokenRepository;
 import com.shyashyashya.refit.global.auth.service.JwtUtil;
-import com.shyashyashya.refit.global.constant.EnvironmentType;
 import com.shyashyashya.refit.global.constant.UrlConstant;
 import com.shyashyashya.refit.global.exception.CustomException;
 import com.shyashyashya.refit.global.oauth2.dto.OAuth2LoginUrlResponse;
 import com.shyashyashya.refit.global.oauth2.dto.OAuth2ResultDto;
 import com.shyashyashya.refit.global.property.OAuth2Property;
 import com.shyashyashya.refit.global.util.CurrentProfile;
-import java.util.Map;
 import java.util.Optional;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatusCode;
@@ -45,23 +42,19 @@ public class GoogleOAuth2Service implements OAuth2Service {
     private final JwtUtil jwtUtil;
     private final RestClient restClient;
 
-    // TODO: 추후 별도의 DB, 또는 Redis에 저장할 수 있도록 구현
-    // TODO: 또는 state 자체를 JWT로 발급해 stateless로 구현하기
-    private final Map<String, EnvironmentType> stateMap = new ConcurrentHashMap<>();
-
     @Override
-    public OAuth2LoginUrlResponse buildOAuth2LoginUrl(EnvironmentType environmentType) {
+    public OAuth2LoginUrlResponse buildOAuth2LoginUrl(String requestHostUrl) {
+        validateRequestHostUrl(requestHostUrl);
         String googleClientId = oauth2Property.google().clientId();
-        String redirectUri = getRedirectUri();
         String scope = String.join(" ", oauth2Property.google().scope());
         String responseType = "code";
 
         String loginUrlResponseUrl = UriComponentsBuilder.fromUriString("https://accounts.google.com/o/oauth2/v2/auth")
                 .queryParam("client_id", googleClientId)
-                .queryParam("redirect_uri", redirectUri)
+                .queryParam("redirect_uri", getRedirectUri())
                 .queryParam("response_type", responseType)
                 .queryParam("scope", scope)
-                .queryParam("state", createState(environmentType))
+                .queryParam("state", jwtUtil.createOAuth2StateToken(requestHostUrl))
                 .toUriString();
         return OAuth2LoginUrlResponse.from(loginUrlResponseUrl);
     }
@@ -69,44 +62,35 @@ public class GoogleOAuth2Service implements OAuth2Service {
     @Transactional
     @Override
     public OAuth2ResultDto handleOAuth2Callback(String code, String state) {
+        var validatedOAuth2StateToken = jwtUtil.getValidatedJwtToken(state);
+        var requestHostUrl = jwtUtil.getRequestHostUrl(validatedOAuth2StateToken);
+        String frontendRedirectUrl = requestHostUrl + UrlConstant.LOGIN_REDIRECT_PATH;
+
         var tokenResponse = fetchAccessToken(code);
         var userInfo = fetchUserInfo(tokenResponse.access_token());
 
-        var userOptional = userRepository.findByEmail(userInfo.email());
-        var userId = userOptional.map(User::getId).orElse(null);
+        Optional<User> userOptional = userRepository.findByEmail(userInfo.email());
+        Long userId = userOptional.map(User::getId).orElse(null);
 
-        var accessToken = jwtUtil.createAccessToken(userInfo.email(), userId);
-        var refreshToken = jwtUtil.createRefreshToken(userInfo.email(), userId);
+        String accessToken = jwtUtil.createAccessToken(userInfo.email(), userId);
+        String refreshToken = jwtUtil.createRefreshToken(userInfo.email(), userId);
 
-        refreshTokenRepository.save(
-                RefreshToken.create(refreshToken, userInfo.email(), jwtUtil.getExpiration(refreshToken)));
-
-        EnvironmentType environmentType = getEnvironmentTypeFromStateOrThrow(state);
-        String frontendRedirectUrl = buildFrontendRedirectUrl(environmentType);
+        var refreshTokenExpiration = jwtUtil.getValidatedJwtToken(refreshToken).getExpiration();
+        refreshTokenRepository.save(RefreshToken.create(refreshToken, userInfo.email(), refreshTokenExpiration));
 
         return userOptional
                 .map(user -> OAuth2ResultDto.createUser(accessToken, refreshToken, user, frontendRedirectUrl))
                 .orElseGet(() -> OAuth2ResultDto.createGuest(accessToken, refreshToken, userInfo, frontendRedirectUrl));
     }
 
-    private String createState(EnvironmentType environmentType) {
-        String state = UUID.randomUUID().toString();
-        stateMap.put(state, environmentType);
-        return state;
-    }
-
-    private EnvironmentType getEnvironmentTypeFromStateOrThrow(String state) {
-        return Optional.ofNullable(stateMap.get(state))
-                .orElseThrow(() -> new IllegalStateException("Unregistered state value: " + state));
-    }
-
-    private String buildFrontendRedirectUrl(EnvironmentType environmentType) {
-        return switch (environmentType) {
-                    case LOCAL -> UrlConstant.LOCAL_CLIENT_URL;
-                    case DEV -> UrlConstant.DEV_CLIENT_URL;
-                    case MAIN -> UrlConstant.MAIN_CLIENT_URL;
-                }
-                + UrlConstant.LOGIN_REDIRECT_PATH;
+    private void validateRequestHostUrl(String requestHostUrl) {
+        switch (requestHostUrl) {
+            case UrlConstant.LOCAL_CLIENT_URL,
+                    UrlConstant.DEV_CLIENT_URL,
+                    UrlConstant.MAIN_CLIENT_URL,
+                    UrlConstant.LOCAL_SERVER_URL -> {}
+            default -> throw new CustomException(REQUEST_HOST_OAUTH_REDIRECTION_NOT_ALLOWED);
+        }
     }
 
     private String getRedirectUri() {
