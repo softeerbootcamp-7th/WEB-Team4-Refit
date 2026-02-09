aeec0add3243bc1a1191b41fd2827aaffcb09233
lja3723 <lja3723@gmail.com>
2026-02-09 23:27:04 +0900

    chore: spotless 적용

--- backend/src/main/java/com/shyashyashya/refit/global/util/RequestHostUrlUtil.java
+++ backend/src/main/java/com/shyashyashya/refit/global/util/RequestHostUrlUtil.java
@@ -1,20 +1,18 @@
 package com.shyashyashya.refit.global.util;
 
 import com.shyashyashya.refit.global.constant.UrlConstant;
-import org.springframework.stereotype.Component;
-
 import java.util.Map;
+import org.springframework.stereotype.Component;
 
 @Component
 public class RequestHostUrlUtil {
 
     public static final Map<String, String> ENV_URL_MAP = Map.of(
-        "LOCAL_SERVER", UrlConstant.LOCAL_SERVER_URL,
-        "LOCAL", UrlConstant.LOCAL_CLIENT_URL,
-        "DEV_SERVER", UrlConstant.DEV_SERVER_URL,
-        "DEV", UrlConstant.DEV_CLIENT_URL,
-        "MAIN", UrlConstant.MAIN_CLIENT_URL
-    );
+            "LOCAL_SERVER", UrlConstant.LOCAL_SERVER_URL,
+            "LOCAL", UrlConstant.LOCAL_CLIENT_URL,
+            "DEV_SERVER", UrlConstant.DEV_SERVER_URL,
+            "DEV", UrlConstant.DEV_CLIENT_URL,
+            "MAIN", UrlConstant.MAIN_CLIENT_URL);
 
     public String getRequestHostUrl(String env) {
         if (env == null || env.isBlank()) {
