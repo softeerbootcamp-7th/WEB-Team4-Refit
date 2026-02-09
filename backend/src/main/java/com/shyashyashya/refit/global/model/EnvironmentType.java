66e9acfa70ed7fbc2dc27f7314e92aff9fb9ef78
lja3723 <lja3723@gmail.com>
2026-02-09 18:59:15 +0900

    refactor: 명세 변경에 따른 수정

--- backend/src/main/java/com/shyashyashya/refit/global/model/EnvironmentType.java
+++ backend/src/main/java/com/shyashyashya/refit/global/model/EnvironmentType.java
@@ -1,9 +1,22 @@
 package com.shyashyashya.refit.global.model;
 
-public enum EnvironmentType {
-    TEST, LOCAL, DEV, MAIN;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
-    public static EnvironmentType fromString(String profile) {
-        return EnvironmentType.valueOf(profile.toUpperCase());
+public enum EnvironmentType {
+    TEST,
+    LOCAL,
+    DEV,
+    MAIN;
+
+    public static EnvironmentType fromString(String environmentType) {
+        try {
+            return EnvironmentType.valueOf(environmentType.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            String availableValues =
+                    Arrays.stream(EnvironmentType.values()).map(Enum::name).collect(Collectors.joining(", "));
+            throw new IllegalArgumentException("Invalid environment type: " + environmentType
+                    + ". Available values are: [" + availableValues + "]");
+        }
     }
 }
