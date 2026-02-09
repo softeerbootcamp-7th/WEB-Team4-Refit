d4f450d7223196957612f6ae89a9362f61dd1100
Everdu <kckc0608@naver.com>
2026-02-09 16:57:43 +0900

    [DEV-146/BE] docs: Controller에 Swagger 어노테이션 추가 (#246)
    * docs: 대시보드 설명 보충
    
    * docs: 면접 API docs 작성
    
    * docs: interview my api docs 작성
    
    * docs: job category api 스웨거 설명 추가
    
    * docs: QnaSet API 스웨거 문서 작성
    
    * docs: 스크랩 폴더 API 스웨거 문서 작성
    
    * docs: Test User API 스웨거 문서 작성
    
    * docs: User API 스웨거 문서 작성
    
    * docs: 스크랩 폴더 이름을 수정하는 API 의 스웨거 설명 작성
    
    * docs: Scrap Folder API 에 대한 스웨거 Tag 추가
    
    * docs: Auth API 에 대한 스웨거 문서 작성
    
    * docs: TEST Auth API 스웨거 문서 작성
    
    * docs: Qna Set My API 스웨거 문서의 Tag 설명을 Interview My Controller 와 비슷하게 수정
    
    * docs: 구글 로그인 API 스웨거 문서 작성
    
    * chore: spotless 작성
    
    * docs: 테스트용 API 에 테스트용 명시
    
    * docs: '작성중인' 표현을 '임시저장'으로 구체적으로 명시
    
    * docs: 스웨거 태그 정렬 로직 작성
    
    * docs: swagger 문서에서 schema 영역이 기본으로 숨겨지도록 설정
    
    * docs: operation 정렬 옵션 설정
    
    * chore: spotless 적용

--- backend/src/main/java/com/shyashyashya/refit/domain/qnaset/api/QnaSetController.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/qnaset/api/QnaSetController.java
@@ -8,6 +8,8 @@ import com.shyashyashya.refit.domain.qnaset.dto.request.PdfHighlightingUpdateReq
 import com.shyashyashya.refit.domain.qnaset.dto.request.QnaSetUpdateRequest;
 import com.shyashyashya.refit.domain.qnaset.dto.response.FrequentQnaSetResponse;
 import com.shyashyashya.refit.domain.qnaset.service.QnaSetService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "QnA Set API", description = "질문 답변 세트 관련 API 입니다.")
 @RestController
 @RequestMapping("/qna-set")
 @RequiredArgsConstructor
@@ -28,14 +31,19 @@ public class QnaSetController {
 
     private final QnaSetService qnaSetService;
 
+    @Operation(
+            summary = "지정한 산업군 / 직무의 빈출 질문 답변 세트를 조회합니다.",
+            description = "지정한 산업군 / 직무의 빈출 질문 답변 세트를 조회합니다. 지정하지 않은 필드에 대해서는 전체를 대상으로 조회합니다.")
     @GetMapping("/frequent")
     public ResponseEntity<CommonResponse<List<FrequentQnaSetResponse>>> getFrequentQuestions(
             @RequestParam Long industryId, @RequestParam Long jobCategoryId) {
+        // TODO : 산업군, 직무 optional 처리 및 List 로 담을 수 있도록 수정
         var body = qnaSetService.getFrequentQuestions(industryId, jobCategoryId);
         var response = CommonResponse.success(COMMON200, body);
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "지정한 질문에 대해 어려웠던 질문 마킹을 등록합니다.")
     @PatchMapping("/{qnaSetId}/difficult/mark")
     public ResponseEntity<CommonResponse<Void>> markDifficultQuestion(@PathVariable Long qnaSetId) {
         qnaSetService.markDifficultQuestion(qnaSetId);
@@ -43,6 +51,7 @@ public class QnaSetController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "지정한 질문에 대해 어려웠던 질문 마킹을 등록 해제 합니다.")
     @PatchMapping("/{qnaSetId}/difficult/unmark")
     public ResponseEntity<CommonResponse<Void>> unmarkDifficultQuestion(@PathVariable Long qnaSetId) {
         qnaSetService.unmarkDifficultQuestion(qnaSetId);
@@ -50,14 +59,17 @@ public class QnaSetController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "지정한 질문 답변 세트의 질문 답변 내용을 수정합니다.", description = "질문 답변 내용 수정은 '기록중' 상태에서만 가능합니다.")
     @PutMapping("/{qnaSetId}")
     public ResponseEntity<CommonResponse<Void>> updateQnaSet(
             @PathVariable Long qnaSetId, @Valid @RequestBody QnaSetUpdateRequest request) {
+        // TODO : 질문 답변 내용 수정이 '기록중' 상태에서만 가능하도록 검증 추가
         qnaSetService.updateQnaSet(qnaSetId, request);
         var response = CommonResponse.success(COMMON200);
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "지정한 질문 답변 세트에 대해 PDF 하이라이팅 정보를 등록/수정합니다.")
     @PutMapping("/{qnaSetId}/pdf-highlightings")
     public ResponseEntity<CommonResponse<Void>> updatePdfHighlighting(
             @PathVariable Long qnaSetId, @Valid @RequestBody List<PdfHighlightingUpdateRequest> request) {
@@ -66,6 +78,7 @@ public class QnaSetController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "지정한 질문 답변 세트에 대해 등록된 PDF 하이라이팅 정보를 조회합니다.")
     @GetMapping("/{qnaSetId}/pdf-highlightings")
     public ResponseEntity<CommonResponse<List<PdfHighlightingDto>>> getPdfHighlightings(@PathVariable Long qnaSetId) {
         var body = qnaSetService.getPdfHighlightings(qnaSetId);
