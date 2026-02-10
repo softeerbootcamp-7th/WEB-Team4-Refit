1cef8fc17c760665a614b1c2e9c363c3d2b20c69
lja3723 <lja3723@gmail.com>
2026-02-10 11:55:04 +0900

    feat: requestUserContext에 이메일을 주입할 수 있도록 함

--- backend/src/main/java/com/shyashyashya/refit/global/util/RequestUserContext.java
+++ backend/src/main/java/com/shyashyashya/refit/global/util/RequestUserContext.java
@@ -5,6 +5,7 @@ import static com.shyashyashya.refit.global.exception.ErrorCode.USER_NOT_FOUND;
 import com.shyashyashya.refit.domain.user.model.User;
 import com.shyashyashya.refit.domain.user.repository.UserRepository;
 import com.shyashyashya.refit.global.exception.CustomException;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -19,14 +20,24 @@ import org.springframework.web.context.annotation.RequestScope;
 public class RequestUserContext {
 
     private final UserRepository userRepository;
+    @Setter
+    private String email;
 
     @Setter
     private Long userId;
 
+    public String getEmail() {
+        if (this.email == null) {
+            log.error("RequestUserContext에 설정된 email이 없습니다.");
+            throw new IllegalStateException("RequestUserContext에 설정된 email이 없습니다.");
+        }
+        return this.email;
+    }
+
     public User getRequestUser() {
         if (userId == null) {
-            log.error("RequestUserContext에 설정된 User가 없습니다.");
-            throw new IllegalStateException("RequestUserContext에 설정된 User가 없습니다.");
+            log.error("RequestUserContext에 설정된 userId가 없습니다.");
+            throw new IllegalStateException("RequestUserContext에 설정된 userId가 없습니다.");
         }
         return userRepository.findById(userId).orElseThrow(() -> new CustomException(USER_NOT_FOUND));
     }
