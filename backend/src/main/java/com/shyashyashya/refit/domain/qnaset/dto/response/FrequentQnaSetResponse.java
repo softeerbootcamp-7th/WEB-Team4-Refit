62de5d145907882a132b95bb60c9ddcec1afeabc
kckc0608 <kckc0608@naver.com>
2026-02-10 21:10:12 +0900

    feat: 프론트 요구사항에 맞춰 response 수정

--- backend/src/main/java/com/shyashyashya/refit/domain/qnaset/dto/response/FrequentQnaSetResponse.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/qnaset/dto/response/FrequentQnaSetResponse.java
@@ -1,11 +1,28 @@
 package com.shyashyashya.refit.domain.qnaset.dto.response;
 
+import com.shyashyashya.refit.domain.interview.model.Interview;
+import com.shyashyashya.refit.domain.interview.model.InterviewType;
 import com.shyashyashya.refit.domain.qnaset.model.QnaSet;
+import lombok.AccessLevel;
+import lombok.Builder;
 
-// TODO : 프론트 개발 화면에 따라 변경 여지 존재
-public record FrequentQnaSetResponse(String question) {
+import java.time.LocalDateTime;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record FrequentQnaSetResponse(String industryName,
+                                     String jobCategoryName,
+                                     InterviewType interviewType,
+                                     LocalDateTime interviewStartAt,
+                                     String question) {
     public static FrequentQnaSetResponse from(QnaSet qnaSet) {
         // TODO : 질문에서 민감 정보 삭제
-        return new FrequentQnaSetResponse(qnaSet.getQuestionText());
+        Interview interview = qnaSet.getInterview();
+        return FrequentQnaSetResponse.builder()
+                .industryName(interview.getIndustry().getName())
+                .jobCategoryName(interview.getJobCategory().getName())
+                .interviewType(interview.getInterviewType())
+                .interviewStartAt(interview.getStartAt())
+                .question(qnaSet.getQuestionText())
+                .build();
     }
 }
