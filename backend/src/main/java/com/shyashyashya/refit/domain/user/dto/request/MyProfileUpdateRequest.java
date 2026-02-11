83b1950fe84d7754b68807d5cb303b72217c876b
lja3723 <lja3723@gmail.com>
2026-02-12 03:36:53 +0900

    feat: UserConstant 도입

--- backend/src/main/java/com/shyashyashya/refit/domain/user/dto/request/MyProfileUpdateRequest.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/user/dto/request/MyProfileUpdateRequest.java
@@ -1,10 +1,11 @@
 package com.shyashyashya.refit.domain.user.dto.request;
 
+import com.shyashyashya.refit.domain.user.constant.UserConstant;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public record MyPageUpdateRequest(
-        @NotBlank @Size(max = 30) String nickname,
+public record MyProfileUpdateRequest(
+        @NotBlank @Size(max = UserConstant.USER_NICKNAME_MAX_LENGTH) String nickname,
         @NotNull Long industryId,
         @NotNull Long jobCategoryId) {}
--- backend/src/main/java/com/shyashyashya/refit/domain/user/dto/request/UserSignUpRequest.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/user/dto/request/UserSignUpRequest.java
@@ -1,14 +1,15 @@
 package com.shyashyashya.refit.domain.user.dto.request;
 
+import com.shyashyashya.refit.domain.user.constant.UserConstant;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record UserSignUpRequest(
-        @Email @NotBlank @Size(max = 255) String email,
+        @Email @NotBlank @Size(max = UserConstant.USER_EMAIL_MAX_LENGTH) String email,
         @NotBlank String env,
-        @NotBlank @Size(max = 30) String nickname,
-        @NotBlank @Size(max = 2048) String profileImageUrl,
+        @NotBlank @Size(max = UserConstant.USER_NICKNAME_MAX_LENGTH) String nickname,
+        @NotBlank @Size(max = UserConstant.USER_PROFILE_IMAGE_URL_MAX_LENGTH) String profileImageUrl,
         @NotNull Long industryId,
         @NotNull Long jobCategoryId) {}
