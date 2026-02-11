7924308f462de1ab8d48179aea061ee78f4872dc
lja3723 <lja3723@gmail.com>
2026-02-12 03:38:24 +0900

    feat: 사용자 닉네임 중복 검증 메서드 추가

--- backend/src/main/java/com/shyashyashya/refit/domain/user/service/validator/UserValidator.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/user/service/validator/UserValidator.java
@@ -1,5 +1,6 @@
 package com.shyashyashya.refit.domain.user.service.validator;
 
+import static com.shyashyashya.refit.global.exception.ErrorCode.USER_NICKNAME_CONFLICT;
 import static com.shyashyashya.refit.global.exception.ErrorCode.USER_SIGNUP_EMAIL_CONFLICT;
 
 import com.shyashyashya.refit.domain.user.repository.UserRepository;
@@ -10,13 +11,19 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class UserSignUpValidator {
+public class UserValidator {
 
     private final UserRepository userRepository;
 
     public void validateEmailConflict(@NonNull String email) {
         if (userRepository.findByEmail(email).isPresent()) {
             throw new CustomException(USER_SIGNUP_EMAIL_CONFLICT);
+        }
+    }
+
+    public void validateNicknameNotConflict(@NonNull String nickname) {
+        if (userRepository.existsByNickname(nickname)) {
+            throw new CustomException(USER_NICKNAME_CONFLICT);
         }
     }
 }
