a27466d5488c684dfc488815a9f06f1984171081
lja3723 <lja3723@gmail.com>
2026-02-12 02:14:39 +0900

    refactor: 파라미터를 TokenPairDto로 받도록 수정

--- backend/src/main/java/com/shyashyashya/refit/global/auth/dto/TestPublishTokenResponse.java
+++ backend/src/main/java/com/shyashyashya/refit/global/auth/dto/TestPublishTokenResponse.java
@@ -6,10 +6,10 @@ import lombok.Builder;
 @Builder(access = AccessLevel.PRIVATE)
 public record TestPublishTokenResponse(boolean isNeedSignUp, TokenPairDto tokens) {
 
-    public static TestPublishTokenResponse of(boolean isNeedSignUp, String accessToken, String refreshToken) {
+    public static TestPublishTokenResponse of(boolean isNeedSignUp, TokenPairDto tokenPair) {
         return TestPublishTokenResponse.builder()
                 .isNeedSignUp(isNeedSignUp)
-                .tokens(TokenPairDto.of(accessToken, refreshToken))
+                .tokens(tokenPair)
                 .build();
     }
 }
