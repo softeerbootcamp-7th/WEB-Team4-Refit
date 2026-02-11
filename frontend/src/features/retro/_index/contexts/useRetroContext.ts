1be2312deacd4683ad1dd061ae04e37689d166e4
HIHJH <2170095@ewhain.net>
2026-02-11 11:35:30 +0900

    refactor: retro 페이지 공통 컴포넌트로 대체 및 context 제거

--- frontend/src/features/retro/_index/contexts/useRetroContext.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react'
-import { RetroContext, type RetroContextType } from './RetroContext'
-
-export function useRetroContext(): RetroContextType {
-  const context = useContext(RetroContext)
-  if (!context) throw new Error('useRetroContext must be used within RetroProvider')
-  return context
-}
