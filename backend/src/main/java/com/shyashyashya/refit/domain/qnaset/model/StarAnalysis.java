42188c737c67f9305dd242bb033c2dbfd9facfbd
zxc534 <zxc534@naver.com>
2026-02-11 10:20:56 +0900

    feat: StarAnalysis 엔티티의 QnaSet 관계를 OneToOne으로 변경

--- backend/src/main/java/com/shyashyashya/refit/domain/qnaset/model/StarAnalysis.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/qnaset/model/StarAnalysis.java
@@ -11,6 +11,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -52,7 +53,7 @@ public class StarAnalysis extends BaseEntity {
     private StarAnalysisStatus status;
 
     @JoinColumn(name = "qna_set_id", nullable = false, unique = true)
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     private QnaSet qnaSet;
 
     /*
