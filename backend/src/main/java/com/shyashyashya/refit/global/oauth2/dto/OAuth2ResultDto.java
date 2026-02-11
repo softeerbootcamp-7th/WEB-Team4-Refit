225b62c15b47e12d82427d8b2668ed431518dbdf
lja3723 <lja3723@gmail.com>
2026-02-11 14:58:35 +0900

    refactor: frontendRedirectUri 대신 ClientOriginType을 저장하도록 수정

--- backend/src/main/java/com/shyashyashya/refit/global/oauth2/dto/OAuth2ResultDto.java
+++ backend/src/main/java/com/shyashyashya/refit/global/oauth2/dto/OAuth2ResultDto.java
@@ -3,21 +3,22 @@ package com.shyashyashya.refit.global.oauth2.dto;
 import com.shyashyashya.refit.domain.user.model.User;
 import com.shyashyashya.refit.global.auth.dto.TokenPairDto;
 import com.shyashyashya.refit.global.oauth2.service.GoogleOAuth2Service.GoogleUserInfo;
+import com.shyashyashya.refit.global.util.ClientOriginType;
 import lombok.Builder;
 
 @Builder
 public record OAuth2ResultDto(
         TokenPairDto tokenPair,
-        String frontRedirectUri,
+        ClientOriginType clientOriginType,
         boolean isNeedSignup,
         String nickname,
         String profileImageUrl) {
 
     public static OAuth2ResultDto createUser(
-            String accessToken, String refreshToken, User user, String frontRedirectUri) {
+            String accessToken, String refreshToken, User user, ClientOriginType clientOriginType) {
         return OAuth2ResultDto.builder()
                 .tokenPair(TokenPairDto.of(accessToken, refreshToken))
-                .frontRedirectUri(frontRedirectUri)
+                .clientOriginType(clientOriginType)
                 .isNeedSignup(false)
                 .nickname(user.getNickname())
                 .profileImageUrl(user.getProfileImageUrl())
@@ -25,10 +26,10 @@ public record OAuth2ResultDto(
     }
 
     public static OAuth2ResultDto createGuest(
-            String accessToken, String refreshToken, GoogleUserInfo userInfo, String frontRedirectUri) {
+            String accessToken, String refreshToken, GoogleUserInfo userInfo, ClientOriginType clientOriginType) {
         return OAuth2ResultDto.builder()
                 .tokenPair(TokenPairDto.of(accessToken, refreshToken))
-                .frontRedirectUri(frontRedirectUri)
+                .clientOriginType(clientOriginType)
                 .isNeedSignup(true)
                 .nickname(userInfo.name())
                 .profileImageUrl(userInfo.picture())
