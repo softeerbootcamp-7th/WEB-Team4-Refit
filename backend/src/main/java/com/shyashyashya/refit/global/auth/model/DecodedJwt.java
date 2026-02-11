8c9bd8e3038ffefa0bf37b0dcb0df1091d40aad1
lja3723 <lja3723@gmail.com>
2026-02-12 02:17:45 +0900

    refactor: decodedJwt를 더 간편하게 만들 수 있도록 함

--- backend/src/main/java/com/shyashyashya/refit/global/auth/model/DecodedJwt.java
+++ backend/src/main/java/com/shyashyashya/refit/global/auth/model/DecodedJwt.java
@@ -1,29 +1,37 @@
 package com.shyashyashya.refit.global.auth.model;
 
+import com.shyashyashya.refit.global.constant.AuthConstant;
 import io.jsonwebtoken.Claims;
-import java.time.Instant;
 import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
 
 @Builder(access = lombok.AccessLevel.PRIVATE)
-public record DecodedJwt(Claims claims, boolean isExpired, DecodedJwtType type) {
+@Slf4j
+public record DecodedJwt(Claims claims, DecodedJwtType type, boolean isExpired) {
 
-    public Instant getExpiration() {
-        return claims.getExpiration().toInstant();
-    }
-
-    public static DecodedJwt createUnexpiredToken(Claims claims, DecodedJwtType type) {
+    public static DecodedJwt createUnexpired(Claims claims) {
         return DecodedJwt.builder()
                 .claims(claims)
                 .isExpired(false)
-                .type(type)
+                .type(extractDecodedJwtTypeOrThrow(claims))
                 .build();
     }
 
-    public static DecodedJwt createExpiredToken(Claims claims, DecodedJwtType type) {
+    public static DecodedJwt createExpired(Claims claims) {
         return DecodedJwt.builder()
                 .claims(claims)
                 .isExpired(true)
-                .type(type)
+                .type(extractDecodedJwtTypeOrThrow(claims))
                 .build();
+    }
+
+    private static DecodedJwtType extractDecodedJwtTypeOrThrow(Claims claims) {
+        try {
+            String jwtTokenType = claims.get(AuthConstant.CLAIM_KEY_JWT_TOKEN_TYPE, String.class);
+            return DecodedJwtType.valueOf(jwtTokenType);
+        } catch (NullPointerException | IllegalArgumentException e) {
+            log.error("Failed to extract JWT token type: {}", e.getMessage());
+            throw new IllegalStateException(e);
+        }
     }
 }
