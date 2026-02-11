e8bf0ab1ab10ddb2a29f8063964a8c4e97edfc55
lja3723 <lja3723@gmail.com>
2026-02-11 15:26:09 +0900

    refactor: /user/signup시 origin을 쿼리파라미터로 받도록 수정

--- backend/src/main/java/com/shyashyashya/refit/domain/user/api/UserController.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/user/api/UserController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "User API", description = "유저와 관련된 API 입니다.")
@@ -37,8 +38,9 @@ public class UserController {
             summary = "새롭게 회원가입 합니다.",
             description = "현재 로그인한 계정이 아직 등록되지 않은 상태일 때만 회원가입 할 수 있습니다. 회원가입이 성공하면 토큰을 새로 발급합니다.")
     @PostMapping("/signup")
-    public ResponseEntity<Void> signUp(@Valid @RequestBody UserSignUpRequest userSignUpRequest) {
-        ClientOriginType originType = ClientOriginType.fromOriginString(userSignUpRequest.origin());
+    public ResponseEntity<Void> signUp(
+            @RequestParam(required = false) String origin, @Valid @RequestBody UserSignUpRequest userSignUpRequest) {
+        ClientOriginType originType = ClientOriginType.fromOriginString(origin);
         TokenPairDto tokenPairDto = userService.signUp(userSignUpRequest);
 
         String accessTokenCookie = cookieUtil.createAccessTokenCookie(tokenPairDto.accessToken(), originType);
@@ -47,7 +49,7 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.FOUND)
                 .header(HttpHeaders.SET_COOKIE, accessTokenCookie)
                 .header(HttpHeaders.SET_COOKIE, refreshTokenCookie)
-                .header(HttpHeaders.LOCATION, ClientOriginType.getClientOriginUrl(userSignUpRequest.origin()))
+                .header(HttpHeaders.LOCATION, ClientOriginType.getClientOriginUrl(origin))
                 .build();
     }
 
