f124fecb21dfbaa6edd17d597499e7c846533b32
HIHJH <2170095@ewhain.net>
2026-02-11 11:27:53 +0900

    feat: 질답 카드 공통 컴포넌트 생성

---
+++ frontend/src/features/_common/components/qnaSet/QnaSetCard.tsx
@@ -0,0 +1,43 @@
+import { CornerDownRightIcon } from '@/shared/assets'
+import { Badge } from '@/shared/components'
+
+type QnaSetCardProps = {
+  idx: number
+  questionText: string
+  answerText: string
+  badgeTheme?: 'orange-100' | 'gray-100'
+  topRightComponent?: React.ReactNode
+  ref?: React.Ref<HTMLDivElement>
+  className?: string
+  children?: React.ReactNode
+}
+
+export function QnaSetCard({
+  idx,
+  questionText,
+  answerText,
+  badgeTheme = 'orange-100',
+  topRightComponent,
+  ref,
+  className = '',
+  children,
+}: QnaSetCardProps) {
+  const questionLabel = `${idx}번 질문`
+
+  return (
+    <div ref={ref} className={`bg-gray-white flex flex-col gap-6 rounded-lg p-5 ${className}`}>
+      <div className="flex flex-col gap-3">
+        <div className="flex flex-1 items-center justify-between">
+          <Badge type="question-label" theme={badgeTheme} content={questionLabel} />
+          {topRightComponent}
+        </div>
+        <span className="title-s-semibold">{questionText}</span>
+        <div className="ml-2 flex gap-4">
+          <CornerDownRightIcon className="h-4 w-4 shrink-0 text-gray-600" />
+          <span>{answerText}</span>
+        </div>
+      </div>
+      {children}
+    </div>
+  )
+}
