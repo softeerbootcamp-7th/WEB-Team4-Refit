58a63c9829b1f9e9ae2b3e17160fb35269d862d7
lja3723 <lja3723@gmail.com>
2026-02-12 20:22:53 +0900

    refactor: QnaSet의 소유권을 검증하는 공통 로직을 private 메소드로 분리
    # Conflicts:
    #	backend/src/main/java/com/shyashyashya/refit/domain/qnaset/service/QnaSetService.java

--- backend/src/main/java/com/shyashyashya/refit/domain/qnaset/service/QnaSetService.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/qnaset/service/QnaSetService.java
@@ -63,59 +63,41 @@ public class QnaSetService {
 
     @Transactional
     public void markDifficultQuestion(Long qnaSetId) {
-        QnaSet qnaSet = qnaSetRepository.findById(qnaSetId).orElseThrow(() -> new CustomException(QNA_SET_NOT_FOUND));
-
-        User requesetUser = requestUserContext.getRequestUser();
-        Interview interview = qnaSet.getInterview();
-        interviewValidator.validateInterviewOwner(interview, requesetUser);
-
+        QnaSet qnaSet = getValidatedQnaSet(qnaSetId);
         qnaSet.markDifficult();
     }
 
     @Transactional
     public void unmarkDifficultQuestion(Long qnaSetId) {
-        QnaSet qnaSet = qnaSetRepository.findById(qnaSetId).orElseThrow(() -> new CustomException(QNA_SET_NOT_FOUND));
-
-        User requesetUser = requestUserContext.getRequestUser();
-        Interview interview = qnaSet.getInterview();
-        interviewValidator.validateInterviewOwner(interview, requesetUser);
-
+        QnaSet qnaSet = getValidatedQnaSet(qnaSetId);
         qnaSet.unmarkDifficult();
     }
 
     @Transactional
     public void updateQnaSet(Long qnaSetId, QnaSetUpdateRequest request) {
-        QnaSet qnaSet = qnaSetRepository.findById(qnaSetId).orElseThrow(() -> new CustomException(QNA_SET_NOT_FOUND));
-
-        User requestUser = requestUserContext.getRequestUser();
-        Interview interview = qnaSet.getInterview();
-        interviewValidator.validateInterviewOwner(interview, requestUser);
-
+        QnaSet qnaSet = getValidatedQnaSet(qnaSetId);
         qnaSet.updateQuestionText(request.questionText());
         qnaSet.updateAnswerText(request.answerText());
         updateOrCreateSelfReview(qnaSet, request.selfReviewText());
     }
 
     @Transactional
+    public void deleteQnaSet(Long qnaSetId) {
+        QnaSet qnaSet = getValidatedQnaSet(qnaSetId);
+        interviewValidator.validateInterviewReviewStatusQnaSetDraft(qnaSet.getInterview());
+        qnaSetRepository.deleteById(qnaSetId);
+    }
+
+    @Transactional
     public void updatePdfHighlighting(Long qnaSetId, List<PdfHighlightingUpdateRequest> request) {
-        QnaSet qnaSet = qnaSetRepository.findById(qnaSetId).orElseThrow(() -> new CustomException(QNA_SET_NOT_FOUND));
-
-        User requestUser = requestUserContext.getRequestUser();
-        Interview interview = qnaSet.getInterview();
-        interviewValidator.validateInterviewOwner(interview, requestUser);
-
+        QnaSet qnaSet = getValidatedQnaSet(qnaSetId);
         deleteAllHighlightingsAndRects(qnaSet);
         saveAllHighlightings(qnaSet, request);
     }
 
     @Transactional(readOnly = true)
     public List<PdfHighlightingDto> getPdfHighlightings(Long qnaSetId) {
-        QnaSet qnaSet = qnaSetRepository.findById(qnaSetId).orElseThrow(() -> new CustomException(QNA_SET_NOT_FOUND));
-
-        User requestUser = requestUserContext.getRequestUser();
-        Interview interview = qnaSet.getInterview();
-        interviewValidator.validateInterviewOwner(interview, requestUser);
-
+        QnaSet qnaSet = getValidatedQnaSet(qnaSetId);
         List<PdfHighlighting> pdfHighlightings = pdfHighlightingRepository.findAllByQnaSet(qnaSet);
         if (pdfHighlightings.isEmpty()) {
             return Collections.emptyList();
@@ -143,6 +125,31 @@ public class QnaSetService {
         return qnaSet;
     }
 
+    @Transactional(readOnly = true)
+    public Page<QnaSetScrapFolderResponse> getMyScrapFoldersWithQnaSetContainingInfo(Long qnaSetId, Pageable pageable) {
+        QnaSet qnaSet = qnaSetRepository.findById(qnaSetId).orElseThrow(() -> new CustomException(QNA_SET_NOT_FOUND));
+
+        User requestUser = requestUserContext.getRequestUser();
+        Interview interview = qnaSet.getInterview();
+        interviewValidator.validateInterviewOwner(interview, requestUser);
+
+        return qnaSetScrapFolderRepository.findAllScrapFoldersWithQnaSetContainingInfo(requestUser, qnaSet, pageable);
+    }
+
+    private List<Long> removeDuplicatedIds(List<Long> list) {
+        if (list == null) {
+            return null;
+        }
+        return list.stream().distinct().toList();
+    }
+
+    private QnaSet getValidatedQnaSet(Long qnaSetId) {
+        QnaSet qnaSet = qnaSetRepository.findById(qnaSetId).orElseThrow(() -> new CustomException(QNA_SET_NOT_FOUND));
+        User requestUser = requestUserContext.getRequestUser();
+        interviewValidator.validateInterviewOwner(qnaSet.getInterview(), requestUser);
+        return qnaSet;
+    }
+
     private void updateOrCreateSelfReview(QnaSet qnaSet, String reqSelfReviewText) {
         if (reqSelfReviewText != null) {
             qnaSetSelfReviewRepository
@@ -151,6 +158,11 @@ public class QnaSetService {
                             selfReview -> selfReview.updateSelfReviewText(reqSelfReviewText),
                             () -> qnaSetSelfReviewRepository.save(QnaSetSelfReview.create(reqSelfReviewText, qnaSet)));
         }
+    }
+
+    private void deleteAllHighlightingsAndRects(QnaSet qnaSet) {
+        pdfHighlightingRectRepository.deleteAllByQnaSet(qnaSet);
+        pdfHighlightingRepository.deleteAllByQnaSet(qnaSet);
     }
 
     private void saveAllHighlightings(QnaSet qnaSet, List<PdfHighlightingUpdateRequest> request) {
@@ -170,38 +182,5 @@ public class QnaSetService {
                         .forEach(pdfHighlightingRectRepository::save);
             }
         });
-    }
-
-    private void deleteAllHighlightingsAndRects(QnaSet qnaSet) {
-        pdfHighlightingRectRepository.deleteAllByQnaSet(qnaSet);
-        pdfHighlightingRepository.deleteAllByQnaSet(qnaSet);
-    }
-
-    @Transactional(readOnly = true)
-    public Page<QnaSetScrapFolderResponse> getMyScrapFoldersWithQnaSetContainingInfo(Long qnaSetId, Pageable pageable) {
-        QnaSet qnaSet = qnaSetRepository.findById(qnaSetId).orElseThrow(() -> new CustomException(QNA_SET_NOT_FOUND));
-
-        User requestUser = requestUserContext.getRequestUser();
-        Interview interview = qnaSet.getInterview();
-        interviewValidator.validateInterviewOwner(interview, requestUser);
-
-        return qnaSetScrapFolderRepository.findAllScrapFoldersWithQnaSetContainingInfo(requestUser, qnaSet, pageable);
-    }
-
-    private List<Long> removeDuplicatedIds(List<Long> list) {
-        if (list == null) {
-            return null;
-        }
-        return list.stream().distinct().toList();
-    }
-
-    @Transactional
-    public void deleteQnaSet(Long qnaSetId) {
-        // TODO qnaSet 소유권 검증 공통 로직 메소드 분리
-        QnaSet qnaSet = qnaSetRepository.findById(qnaSetId).orElseThrow(() -> new CustomException(QNA_SET_NOT_FOUND));
-        User requestUser = requestUserContext.getRequestUser();
-        interviewValidator.validateInterviewOwner(qnaSet.getInterview(), requestUser);
-        interviewValidator.validateInterviewReviewStatusQnaSetDraft(qnaSet.getInterview());
-        qnaSetRepository.deleteById(qnaSetId);
     }
 }
