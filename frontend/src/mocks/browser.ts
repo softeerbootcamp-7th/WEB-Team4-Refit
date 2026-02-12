6c1893223f58b5fd84d737968d11d3b2fb3423a8
forhyundaisofteer <forhyundaisofteer@gmail.com>
2026-02-12 11:33:12 +0900

    feat: mock 로직 추가

--- frontend/src/mocks/browser.ts
+++ frontend/src/mocks/browser.ts
@@ -6,7 +6,9 @@ import {
   getGetMyDifficultQnaSetsMockHandler,
   getGetUpcomingInterviewsMockHandler,
 } from '@/apis/generated/dashboard-api/dashboard-api.msw'
+import { getUpdateRawTextMockHandler } from '@/apis/generated/interview-api/interview-api.msw'
 import { debriefIncompletedMock } from '@/mocks/data/debrief-incompleted'
+import { updateRawTextMock } from '@/mocks/data/update-raw-text'
 
 export const worker = setupWorker(
   getGetMyDifficultQnaSetsMockHandler(),
@@ -14,4 +16,5 @@ export const worker = setupWorker(
   getGetDebriefIncompletedInterviewsMockHandler(debriefIncompletedMock),
   getGetDashboardHeadlineMockHandler(),
   getGetDashboardCalendarInterviewsMockHandler(),
+  getUpdateRawTextMockHandler(updateRawTextMock),
 )
