bc9dcc737facc21e68236f416ce6488e9cb3cb6b
HIHJH <2170095@ewhain.net>
2026-02-13 12:00:31 +0900

    feat: 회고 페이지 mock-api 등록

--- frontend/src/mocks/browser.ts
+++ frontend/src/mocks/browser.ts
@@ -7,12 +7,20 @@ import {
   getGetUpcomingInterviewsMockHandler,
 } from '@/apis/generated/dashboard-api/dashboard-api.msw'
 import {
-  getUpdateRawTextMockHandler,
   getGetInterviewFullMockHandler,
+  getUpdateKptSelfReviewMockHandler,
+  getUpdateRawTextMockHandler,
 } from '@/apis/generated/interview-api/interview-api.msw'
-import { getUpdateQnaSetMockHandler } from '@/apis/generated/qna-set-api/qna-set-api.msw'
+import {
+  getCreateStarAnalysisMockHandler,
+  getGetScrapFoldersContainingQnaSetMockHandler,
+  getUpdateQnaSetMockHandler,
+} from '@/apis/generated/qna-set-api/qna-set-api.msw'
+import { getCreateScrapFolderMockHandler } from '@/apis/generated/scrap-folder-api/scrap-folder-api.msw'
 import { debriefIncompletedMock } from '@/mocks/data/debrief-incompleted'
 import { mockInterviewFull } from '@/mocks/data/interview-full'
+import { mockScrapFolders } from '@/mocks/data/scrap-folders'
+import { mockStarAnalysis } from '@/mocks/data/star-analysis'
 import { updateRawTextMock } from '@/mocks/data/update-raw-text'
 
 export const worker = setupWorker(
@@ -24,4 +32,8 @@ export const worker = setupWorker(
   getUpdateRawTextMockHandler(updateRawTextMock),
   getGetInterviewFullMockHandler(mockInterviewFull),
   getUpdateQnaSetMockHandler({ isSuccess: true, code: 'SUCCESS', message: 'mock update qna set' }),
+  getUpdateKptSelfReviewMockHandler({ isSuccess: true, code: 'SUCCESS', message: 'mock update kpt' }),
+  getCreateStarAnalysisMockHandler(mockStarAnalysis),
+  getGetScrapFoldersContainingQnaSetMockHandler(mockScrapFolders),
+  getCreateScrapFolderMockHandler({ isSuccess: true, code: 'SUCCESS', message: 'mock create scrap folder' }),
 )
