90c60c3e9a680bfdf8c50aa04a70098df4cdfad6
zxc534 <zxc534@naver.com>
2026-02-13 09:26:48 +0900

    test: 인터뷰 상태에 검증 추가로 테스트 로직 수정

--- backend/src/test/java/com/shyashyashya/refit/integration/qnaset/QnaSetIntegrationTest.java
+++ backend/src/test/java/com/shyashyashya/refit/integration/qnaset/QnaSetIntegrationTest.java
@@ -3,6 +3,7 @@ package com.shyashyashya.refit.integration.qnaset;
 import static com.shyashyashya.refit.global.model.ResponseCode.COMMON200;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.in;
 import static org.hamcrest.Matchers.notNullValue;
 
 import com.shyashyashya.refit.core.IntegrationTest;
@@ -10,22 +11,30 @@ import com.shyashyashya.refit.domain.interview.dto.request.InterviewCreateReques
 import com.shyashyashya.refit.domain.interview.dto.request.QnaSetCreateRequest;
 import com.shyashyashya.refit.domain.interview.model.Interview;
 import com.shyashyashya.refit.domain.interview.model.InterviewType;
+import com.shyashyashya.refit.domain.interview.repository.InterviewRepository;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
 
 public class QnaSetIntegrationTest extends IntegrationTest {
 
+    @Autowired
+    private InterviewRepository interviewRepository;
+
     @Nested
     class 질답_세트_생성_시 {
 
         @Test
-        void 질답_세트_생성에_성공한다() {
+        void 인터뷰가_QNA_SET_DRAFT_상태이면_질답_세트_생성에_성공한다() {
             // given
              InterviewCreateRequest interviewCreateRequest = new InterviewCreateRequest(
                                 LocalDateTime.of(2025, 12, 29, 10, 0, 0), InterviewType.FIRST, "현대자동차", 1L, 1L, "BE Developer");
             Interview interview1 = createInterview(interviewCreateRequest);
+            interview1.startLogging();
+            interview1.completeLogging();
+            interviewRepository.save(interview1);
             QnaSetCreateRequest request = new QnaSetCreateRequest("test question text", "test answer text");
 
             // when & then
