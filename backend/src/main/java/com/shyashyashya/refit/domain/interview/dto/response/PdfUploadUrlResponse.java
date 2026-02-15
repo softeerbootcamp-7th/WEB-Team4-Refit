f27e3e27250ecbc3c47c7c74e1ace402f042058f
zxc534 <zxc534@naver.com>
2026-02-15 18:37:55 +0900

    chore: spotless 적용

--- backend/src/main/java/com/shyashyashya/refit/domain/interview/dto/response/PdfUploadUrlResponse.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/interview/dto/response/PdfUploadUrlResponse.java
@@ -2,4 +2,5 @@ package com.shyashyashya.refit.domain.interview.dto.response;
 
 import jakarta.validation.constraints.NotNull;
 
-public record PdfUploadUrlResponse(@NotNull String url, @NotNull String key) {}
+public record PdfUploadUrlResponse(
+        @NotNull String url, @NotNull String key) {}
