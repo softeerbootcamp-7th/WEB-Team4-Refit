70711457dd7a0b1dd80638b45ed6b7c787597d77
HIHJH <2170095@ewhain.net>
2026-02-15 13:51:57 +0900

    feat: 내 질문 모아보기 mock-api 추가

--- frontend/src/mocks/browser.ts
+++ frontend/src/mocks/browser.ts
@@ -18,14 +18,40 @@ import {
   getUpdatePdfHighlightingMockHandler,
   getUpdateQnaSetMockHandler,
 } from '@/apis/generated/qna-set-api/qna-set-api.msw'
+import {
+  getSearchMyQnaSetMockHandler,
+  getGetMyFrequentQnaSetCategoriesMockHandler,
+  getGetMyFrequentQnaSetCategoryQuestionsMockHandler,
+} from '@/apis/generated/qna-set-my-controller/qna-set-my-controller.msw'
+import type { QnaSetSearchRequest } from '@/apis/generated/refit-api.schemas'
 import { getCreateScrapFolderMockHandler } from '@/apis/generated/scrap-folder-api/scrap-folder-api.msw'
 import { debriefIncompletedMock } from '@/mocks/data/debrief-incompleted'
 import { mockInterviewFull } from '@/mocks/data/interview-full'
+import {
+  getMockFrequentCategoryQuestions,
+  getMockSearchMyQnaSet,
+  mockFrequentQuestionCategories,
+} from '@/mocks/data/my-frequent-questions'
 import { mockScrapFolders } from '@/mocks/data/scrap-folders'
 import { mockStarAnalysis } from '@/mocks/data/star-analysis'
 import { updateRawTextMock } from '@/mocks/data/update-raw-text'
 
 export const worker = setupWorker(
+  getGetMyFrequentQnaSetCategoriesMockHandler(mockFrequentQuestionCategories),
+  getGetMyFrequentQnaSetCategoryQuestionsMockHandler((info) => {
+    const categoryId = Number(info.params.categoryId)
+    const url = new URL(info.request.url)
+    const page = Number(url.searchParams.get('page') ?? '0')
+    const size = Number(url.searchParams.get('size') ?? '9')
+    return getMockFrequentCategoryQuestions(categoryId, page, size)
+  }),
+  getSearchMyQnaSetMockHandler(async (info) => {
+    const url = new URL(info.request.url)
+    const page = Number(url.searchParams.get('page') ?? '0')
+    const size = Number(url.searchParams.get('size') ?? '9')
+    const request = (await info.request.json().catch(() => ({}))) as QnaSetSearchRequest
+    return getMockSearchMyQnaSet({ page, size, request })
+  }),
   getGetMyDifficultQnaSetsMockHandler(),
   getGetUpcomingInterviewsMockHandler(),
   getGetDebriefIncompletedInterviewsMockHandler(debriefIncompletedMock),
