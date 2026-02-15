e3a90377a3e6959ca5902b35088b7023f7d07509
lja3723 <lja3723@gmail.com>
2026-02-15 23:10:18 +0900

    feat: 일부만 검색해도 완전한 검색이 되는 검색 Query 작성

--- backend/src/main/java/com/shyashyashya/refit/domain/company/repository/CompanyRepository.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/company/repository/CompanyRepository.java
@@ -1,9 +1,26 @@
 package com.shyashyashya.refit.domain.company.repository;
 
+import com.shyashyashya.refit.domain.company.api.response.CompanyResponse;
 import com.shyashyashya.refit.domain.company.model.Company;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
     Optional<Company> findByName(String name);
+
+    @Query(value = """
+        SELECT new com.shyashyashya.refit.domain.company.api.response.CompanyResponse(c.id, c.name)
+          FROM Company c
+         WHERE c.searchName LIKE CONCAT(:query, '%')
+           AND c.isSearchAllowed = TRUE
+    """, countQuery = """
+        SELECT COUNT(c)
+          FROM Company c
+         WHERE c.searchName LIKE CONCAT(:query, '%')
+           AND c.isSearchAllowed = TRUE
+    """)
+    Page<CompanyResponse> findAllBySearchQuery(String query, Pageable pageable);
 }
