36ea57748c38e069bbc02d557b5db1597539e247
lja3723 <lja3723@gmail.com>
2026-02-16 17:31:18 +0900

    chore: application-gemini.yml을 ai 관련 설정을 담도록 일반화

--- backend/src/main/java/com/shyashyashya/refit/global/property/GeminiProperty.java
+++ backend/src/main/java/com/shyashyashya/refit/global/property/GeminiProperty.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
-@ConfigurationProperties(prefix = "spring.gemini")
+@ConfigurationProperties(prefix = "spring.ai.gemini")
 @Validated
 public record GeminiProperty(
         @NotBlank String apiKey, @NotBlank String promptPath) {}
