75d83d2ba6e31e2210fc40dbe68e621496fbf054
kckc0608 <kckc0608@naver.com>
2026-02-16 16:01:29 +0900

    test: 테스트 패키지를 컨벤션에 맞춰 통일

--- backend/src/test/java/com/shyashyashya/refit/fixture/CompanyFixture.java
+++
@@ -1,8 +0,0 @@
-package com.shyashyashya.refit.fixture;
-
-import com.shyashyashya.refit.domain.company.model.Company;
-
-public class CompanyFixture {
-
-    public static final Company TEST_COMPANY = Company.create("test company", "test.com/logo.png", true);
-}
