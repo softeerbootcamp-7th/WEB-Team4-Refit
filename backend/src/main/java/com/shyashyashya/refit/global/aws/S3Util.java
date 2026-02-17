4f580423ede23ea074bb8512ad8b498e7b9f4531
zxc534 <zxc534@naver.com>
2026-02-17 15:37:14 +0900

    refactor: S3 유틸리티를 범용적 사용이 가능하도록 변경

--- backend/src/main/java/com/shyashyashya/refit/global/aws/S3Util.java
+++ backend/src/main/java/com/shyashyashya/refit/global/aws/S3Util.java
@@ -1,14 +1,14 @@
 package com.shyashyashya.refit.global.aws;
 
-import static com.shyashyashya.refit.global.exception.ErrorCode.INTERVIEW_PDF_DELETE_FAILED;
+import static com.shyashyashya.refit.global.exception.ErrorCode.S3_RESOURCE_DELETE_FAILED;
 
-import com.shyashyashya.refit.domain.interview.dto.response.PresignedUrlResponse;
+import com.shyashyashya.refit.domain.interview.dto.response.PresignedUrlDto;
 import com.shyashyashya.refit.global.exception.CustomException;
 import com.shyashyashya.refit.global.property.S3Property;
 import java.time.Duration;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
@@ -26,19 +26,15 @@ import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignReques
 @Slf4j
 public class S3Util {
 
-    private static final String PDF_EXTENSION = ".pdf";
-
     private final S3Presigner s3Presigner;
     private final S3Client s3Client;
     private final S3Property s3Property;
 
-    public PresignedUrlResponse createPdfUploadUrl() {
-        String key = s3Property.prefix() + UUID.randomUUID() + PDF_EXTENSION;
-
+    public PresignedUrlDto createResourceUploadUrl(String resourceKey, MediaType mediaType) {
         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                 .bucket(s3Property.bucket())
-                .key(key)
-                .contentType("application/pdf")
+                .key(resourceKey)
+                .contentType(mediaType.getType())
                 .build();
 
         PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
@@ -48,13 +44,14 @@ public class S3Util {
 
         PresignedPutObjectRequest presigned = s3Presigner.presignPutObject(presignRequest);
 
-        return new PresignedUrlResponse(presigned.url().toString(), key, s3Property.presignExpireSeconds());
+        return new PresignedUrlDto(presigned.url().toString(), resourceKey, s3Property.presignExpireSeconds());
     }
 
-    public PresignedUrlResponse createPdfDownloadUrl(String key) {
-
-        GetObjectRequest getObjectRequest =
-                GetObjectRequest.builder().bucket(s3Property.bucket()).key(key).build();
+    public PresignedUrlDto createResourceDownloadUrl(String resourceKey) {
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(s3Property.bucket())
+                .key(resourceKey)
+                .build();
 
         GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
                 .signatureDuration(Duration.ofSeconds(s3Property.presignExpireSeconds()))
@@ -63,7 +60,7 @@ public class S3Util {
 
         PresignedGetObjectRequest presigned = s3Presigner.presignGetObject(presignRequest);
 
-        return new PresignedUrlResponse(presigned.url().toString(), key, s3Property.presignExpireSeconds());
+        return new PresignedUrlDto(presigned.url().toString(), resourceKey, s3Property.presignExpireSeconds());
     }
 
     public void deleteFile(String key) {
@@ -74,7 +71,7 @@ public class S3Util {
                     .build());
         } catch (S3Exception e) {
             log.error(e.getMessage(), e);
-            throw new CustomException(INTERVIEW_PDF_DELETE_FAILED);
+            throw new CustomException(S3_RESOURCE_DELETE_FAILED);
         }
     }
 }
