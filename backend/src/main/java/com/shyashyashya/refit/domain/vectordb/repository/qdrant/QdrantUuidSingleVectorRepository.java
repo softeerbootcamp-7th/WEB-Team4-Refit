d8a51b23b62b5f10871e8bbca111641de0567610
lja3723 <lja3723@gmail.com>
2026-02-18 14:57:16 +0900

    feat: 키 타입이 UUID(String)인 커스텀 SingleVectorDocument를 저장할 수 있는 QdrantSingleVectorRepository를 도입

---
+++ backend/src/main/java/com/shyashyashya/refit/domain/vectordb/repository/qdrant/QdrantUuidSingleVectorRepository.java
@@ -0,0 +1,29 @@
+package com.shyashyashya.refit.domain.vectordb.repository.qdrant;
+
+import com.shyashyashya.refit.domain.vectordb.model.SingleVectorDocument;
+import com.shyashyashya.refit.global.property.QdrantProperty;
+import io.qdrant.client.PointIdFactory;
+import io.qdrant.client.QdrantClient;
+import io.qdrant.client.grpc.Common;
+import java.util.UUID;
+
+/**
+ * Id 타입이 UUID(String)인 Single Vector를 저장하는 QdrantRepository
+ */
+public abstract class QdrantUuidSingleVectorRepository<V extends SingleVectorDocument<String>>
+        extends QdrantSingleVectorRepository<String, V> {
+
+    public QdrantUuidSingleVectorRepository(QdrantClient qdrantClient, QdrantProperty qdrantProperty) {
+        super(qdrantClient, qdrantProperty);
+    }
+
+    @Override
+    protected final Common.PointId convertIdToPointId(String id) {
+        return PointIdFactory.id(UUID.fromString(id));
+    }
+
+    @Override
+    protected final String convertPointIdToId(Common.PointId pointId) {
+        return pointId.getUuid();
+    }
+}
