e570cdcb2e9a0e5eb9cc7a4a191fc7389daffc19
lja3723 <lja3723@gmail.com>
2026-02-18 15:01:27 +0900

    feat: QnaSetQuestionVectorRepository의 부모 시그니처 수정

--- backend/src/main/java/com/shyashyashya/refit/domain/vectordb/repository/QnaSetQuestionVectorRepository.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/vectordb/repository/QnaSetQuestionVectorRepository.java
@@ -1,11 +1,15 @@
 package com.shyashyashya.refit.domain.vectordb.repository;
 
+import com.shyashyashya.refit.domain.vectordb.model.SingleVectorDocument;
+import com.shyashyashya.refit.domain.vectordb.repository.qdrant.QdrantLongSingleVectorRepository;
 import com.shyashyashya.refit.global.property.QdrantProperty;
 import io.qdrant.client.QdrantClient;
+import java.util.List;
+import java.util.Map;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class QnaSetQuestionVectorRepository extends QdrantSingleVectorRepository {
+public class QnaSetQuestionVectorRepository extends QdrantLongSingleVectorRepository<SingleVectorDocument<Long>> {
 
     public QnaSetQuestionVectorRepository(QdrantClient qdrantClient, QdrantProperty qdrantProperty) {
         super(qdrantClient, qdrantProperty);
@@ -14,5 +18,11 @@ public class QnaSetQuestionVectorRepository extends QdrantSingleVectorRepository
     @Override
     protected String getCollectionContextName() {
         return "qna-set-question";
+    }
+
+    @Override
+    protected SingleVectorDocument<Long> createSingleVectorDocument(
+            Long id, Map<String, Object> payload, List<Float> vector) {
+        return SingleVectorDocument.of(id, payload, vector);
     }
 }
