015d4b9cce12fb73ef5a1286c64e4596d3a5d2bc
lja3723 <lja3723@gmail.com>
2026-02-19 17:21:06 +0900

    feat: 이미 약관에 동의하면 약관 재동의가 안되도록 수정

--- backend/src/main/java/com/shyashyashya/refit/domain/user/service/validator/UserValidator.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/user/service/validator/UserValidator.java
@@ -1,7 +1,10 @@
 package com.shyashyashya.refit.domain.user.service.validator;
 
+import static com.shyashyashya.refit.global.exception.ErrorCode.USER_ALREADY_AGREED_TO_TERMS;
 import static com.shyashyashya.refit.global.exception.ErrorCode.USER_NICKNAME_CONFLICT;
 import static com.shyashyashya.refit.global.exception.ErrorCode.USER_SIGNUP_EMAIL_CONFLICT;
+
+import com.shyashyashya.refit.domain.user.model.User;
 
 import com.shyashyashya.refit.domain.user.repository.UserRepository;
 import com.shyashyashya.refit.global.exception.CustomException;
@@ -24,6 +27,12 @@ public class UserValidator {
     public void validateNicknameNotConflict(@NonNull String nickname) {
         if (userRepository.existsByNickname(nickname)) {
             throw new CustomException(USER_NICKNAME_CONFLICT);
+        }
+    }
+
+    public void validateUserNotAgreedToTerms(User user) {
+        if (user.isAgreedToTerms()) {
+            throw new CustomException(USER_ALREADY_AGREED_TO_TERMS);
         }
     }
 }
