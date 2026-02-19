d027929b43eff6c1e0b13c3a7ad26abc77225290
kckc0608 <kckc0608@naver.com>
2026-02-19 12:22:01 +0900

    feat: gemini 유틸 작성

---
+++ backend/src/main/java/com/shyashyashya/refit/global/util/GeminiUtil.java
@@ -0,0 +1,44 @@
+package com.shyashyashya.refit.global.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shyashyashya.refit.global.exception.CustomException;
+import com.shyashyashya.refit.global.gemini.GeminiClient;
+import com.shyashyashya.refit.global.gemini.GenerateModel;
+import com.shyashyashya.refit.global.gemini.dto.GeminiGenerateRequest;
+import com.shyashyashya.refit.global.gemini.dto.GeminiGenerateResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import static com.shyashyashya.refit.global.exception.ErrorCode.GEMINI_RESPONSE_PARSING_FAILED;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class GeminiUtil {
+
+    private final GeminiClient geminiClient;
+    private final ObjectMapper objectMapper;
+
+    public <T> T sendTextGenerateRequest(String prompt, Class<T> responseType) {
+        GeminiGenerateRequest requestBody = GeminiGenerateRequest.from(prompt);
+        GeminiGenerateResponse response =
+                geminiClient.sendTextGenerateRequest(requestBody, GenerateModel.GEMMA_3_27B_IT);
+        T result = parseGeminiResponse(response, responseType);
+
+        log.debug("[createCategories] gemini response: {}", result);
+        return result;
+    }
+
+    private <T> T parseGeminiResponse(GeminiGenerateResponse response, Class<T> targetClass) {
+        try {
+            String jsonText = response.firstJsonText()
+                    .orElseThrow(() -> new CustomException(GEMINI_RESPONSE_PARSING_FAILED));
+            log.info("[parseGeminiResponse] raw json text: {}", jsonText);
+            return objectMapper.readValue(jsonText, targetClass);
+        } catch (JsonProcessingException e) {
+            throw new CustomException(GEMINI_RESPONSE_PARSING_FAILED);
+        }
+    }
+}
