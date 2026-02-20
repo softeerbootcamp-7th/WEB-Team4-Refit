fac7396ea61b4872afa01a2b61762884660f152b
zxc534 <zxc534@naver.com>
2026-02-20 23:53:18 +0900

    feat: 질문 답변 세트 생성 및 수정 요청의 검증 로직 수정

--- backend/src/main/java/com/shyashyashya/refit/domain/interview/dto/request/QnaSetCreateRequest.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/interview/dto/request/QnaSetCreateRequest.java
@@ -1,10 +1,12 @@
 package com.shyashyashya.refit.domain.interview.dto.request;
 
+import static com.shyashyashya.refit.domain.qnaset.constant.QnaSetConstant.ANSWER_TEXT_MAX_LENGTH;
 import static com.shyashyashya.refit.domain.qnaset.constant.QnaSetConstant.QUESTION_TEXT_MAX_LENGTH;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record QnaSetCreateRequest(
         @NotBlank @Size(max = QUESTION_TEXT_MAX_LENGTH) String questionText,
-        @NotBlank String answerText) {}
+        @NotNull @Size(max = ANSWER_TEXT_MAX_LENGTH) String answerText) {}
--- backend/src/main/java/com/shyashyashya/refit/domain/qnaset/dto/request/QnaSetUpdateRequest.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/qnaset/dto/request/QnaSetUpdateRequest.java
@@ -3,10 +3,8 @@ package com.shyashyashya.refit.domain.qnaset.dto.request;
 import static com.shyashyashya.refit.domain.qnaset.constant.QnaSetConstant.ANSWER_TEXT_MAX_LENGTH;
 import static com.shyashyashya.refit.domain.qnaset.constant.QnaSetConstant.QUESTION_TEXT_MAX_LENGTH;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record QnaSetUpdateRequest(
-        @NotBlank @Size(max = QUESTION_TEXT_MAX_LENGTH) String questionText,
-        @NotNull @Size(max = ANSWER_TEXT_MAX_LENGTH) String answerText) {}
+        @Size(max = QUESTION_TEXT_MAX_LENGTH) String questionText,
+        @Size(max = ANSWER_TEXT_MAX_LENGTH) String answerText) {}
