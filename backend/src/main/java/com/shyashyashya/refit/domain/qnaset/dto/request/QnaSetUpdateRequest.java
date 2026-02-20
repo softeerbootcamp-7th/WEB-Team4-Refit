b1ba3dc6401ecc4ff60dcd84e8b0ea27abc00883
zxc534 <zxc534@naver.com>
2026-02-20 23:33:06 +0900

    feat: 질문 답변 수정 요청 입력값 검증 로직 수정

--- backend/src/main/java/com/shyashyashya/refit/domain/qnaset/dto/request/QnaSetUpdateRequest.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/qnaset/dto/request/QnaSetUpdateRequest.java
@@ -1,8 +1,12 @@
 package com.shyashyashya.refit.domain.qnaset.dto.request;
 
+import static com.shyashyashya.refit.domain.qnaset.constant.QnaSetConstant.ANSWER_TEXT_MAX_LENGTH;
 import static com.shyashyashya.refit.domain.qnaset.constant.QnaSetConstant.QUESTION_TEXT_MAX_LENGTH;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record QnaSetUpdateRequest(
-        @Size(max = QUESTION_TEXT_MAX_LENGTH) String questionText, String answerText) {}
+        @NotBlank @Size(max = QUESTION_TEXT_MAX_LENGTH) String questionText,
+        @NotNull @Size(max = ANSWER_TEXT_MAX_LENGTH) String answerText) {}
