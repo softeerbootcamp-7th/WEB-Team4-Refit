10ac36aa23c2bae8a567f3244a6b5ef60142cd66
lja3723 <lja3723@gmail.com>
2026-02-22 23:35:33 +0900

    chore: test시 빈 불러오기 제외

--- backend/src/main/java/com/shyashyashya/refit/global/config/QdrantConfig.java
+++ backend/src/main/java/com/shyashyashya/refit/global/config/QdrantConfig.java
@@ -6,7 +6,9 @@ import io.qdrant.client.QdrantGrpcClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("!test")
 @Configuration
 @RequiredArgsConstructor
 public class QdrantConfig {
