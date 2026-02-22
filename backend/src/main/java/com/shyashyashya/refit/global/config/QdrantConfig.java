64e18f60efd7b89f194bb6a212e4412a6a332b04
lja3723 <lja3723@gmail.com>
2026-02-22 17:10:25 +0900

    chore: 필요 없어진 어노테이션 제거

--- backend/src/main/java/com/shyashyashya/refit/global/config/QdrantConfig.java
+++ backend/src/main/java/com/shyashyashya/refit/global/config/QdrantConfig.java
@@ -6,9 +6,7 @@ import io.qdrant.client.QdrantGrpcClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Profile("!test") // test가 아닌 환경에서만 로드됨
 @Configuration
 @RequiredArgsConstructor
 public class QdrantConfig {
