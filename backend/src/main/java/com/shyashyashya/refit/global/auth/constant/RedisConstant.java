718eda174465fb87fa32a9ddec598e6dbe500b5d
lja3723 <lja3723@gmail.com>
2026-02-23 00:50:06 +0900

    refactor: 필요 없어진 파일 제거

--- backend/src/main/java/com/shyashyashya/refit/global/auth/constant/RedisConstant.java
+++
@@ -1,7 +0,0 @@
-package com.shyashyashya.refit.global.auth.constant;
-
-public class RedisConstant {
-
-    public static final String REFRESH_TOKEN_PREFIX = "RT:";
-    public static final String REFRESH_TOKEN_EMAIL_PREFIX = "RT_EMAIL:";
-}
