b4a31169d2b2266da008c1db1cceea7277e14c2a
lja3723 <lja3723@gmail.com>
2026-02-23 02:19:23 +0900

    feat: 질문 임베딩 벡터 분류 시 카테고리 임계값 지정

--- backend/src/main/java/com/shyashyashya/refit/domain/qnaset/constant/QnaSetConstant.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/qnaset/constant/QnaSetConstant.java
@@ -1,11 +1,14 @@
 package com.shyashyashya.refit.domain.qnaset.constant;
 
-public final class QnaSetConstant {
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
-    private QnaSetConstant() {}
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class QnaSetConstant {
 
     public static final int QUESTION_TEXT_MAX_LENGTH = 200;
     public static final int ANSWER_TEXT_MAX_LENGTH = 10_000;
     public static final int QNA_SET_REVIEW_TEXT_MAX_LENGTH = 500;
     public static final int HIGHLIGHTING_TEXT_MAX_LENGTH = 2_000;
+    public static final float CATEGORY_SIMILARITY_THRESHOLD = 0.7f;
 }
