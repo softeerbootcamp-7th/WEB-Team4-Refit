e5ce1d7695a7c63f52e03991f74ae62a19b9dc0e
lja3723 <lja3723@gmail.com>
2026-02-23 03:28:37 +0900

    feat: 질답 세트 삭제시 발생하는 임베딩 삭제 이벤트 정의

---
+++ backend/src/main/java/com/shyashyashya/refit/domain/qnaset/event/QuestionEmbeddingDeletionEvent.java
@@ -0,0 +1,13 @@
+package com.shyashyashya.refit.domain.qnaset.event;
+
+import lombok.Builder;
+
+@Builder(access = lombok.AccessLevel.PRIVATE)
+public record QuestionEmbeddingDeletionEvent(Long qnaSetId) {
+
+    public static QuestionEmbeddingDeletionEvent of(Long qnaSetId) {
+        return QuestionEmbeddingDeletionEvent.builder()
+                .qnaSetId(qnaSetId)
+                .build();
+    }
+}
