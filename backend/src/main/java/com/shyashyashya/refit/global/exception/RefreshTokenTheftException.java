d60ef81af4a79e4a254f82dae2dd458765b999dc
lja3723 <lja3723@gmail.com>
2026-02-23 21:30:45 +0900

    refactor: 리프레시 토큰 탈취 예외 발생시 OriginType 전달

--- backend/src/main/java/com/shyashyashya/refit/global/exception/RefreshTokenTheftException.java
+++ backend/src/main/java/com/shyashyashya/refit/global/exception/RefreshTokenTheftException.java
@@ -1,3 +1,11 @@
 package com.shyashyashya.refit.global.exception;
 
-public class RefreshTokenTheftException extends RuntimeException {}
+import com.shyashyashya.refit.global.model.ClientOriginType;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class RefreshTokenTheftException extends RuntimeException {
+    private final ClientOriginType originType;
+}
