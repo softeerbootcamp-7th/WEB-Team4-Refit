341884168c12e5d9e6ada30315eb87a67c14e437
lja3723 <lja3723@gmail.com>
2026-02-23 10:59:45 +0900

    feat: PR 리뷰 반영

--- backend/src/main/java/com/shyashyashya/refit/global/auth/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/shyashyashya/refit/global/auth/repository/RefreshTokenRepository.java
@@ -3,9 +3,7 @@ package com.shyashyashya.refit.global.auth.repository;
 import com.shyashyashya.refit.global.auth.model.RefreshToken;
 import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 
     Optional<RefreshToken> findByEmail(String email);
