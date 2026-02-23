25b571da7d78dfa78691887142a4f7a8e5d09408
Jangan Lee <lja3723@gmail.com>
2026-02-23 12:02:21 +0900

    [DEV-359/BE] fix: 사용자 수정시 무조건 수정되어야 하는 문제 해결 (#562)
    * feat: 각 파라미터는 이제 null이 아닐 때만 수정되도록 함
    
    * feat: nickname이 null이면 유효성 검사 하지 않도록 함
    
    * feat: industryId, jobCategoryId가 각각 null인 경우 업데이트되지 않도록 수정
    
    * feat: 프로필 수정 요청의 각 필드가 nullable 하도록 수정
    
    * test: 변경된 곳에 대한 테스트 추가
    
    * chore: dev 프로필에서 sql 로깅 제거
    
    * feat: PR 리뷰 반영

--- backend/src/main/java/com/shyashyashya/refit/domain/user/dto/request/MyProfileUpdateRequest.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/user/dto/request/MyProfileUpdateRequest.java
@@ -1,12 +1,12 @@
 package com.shyashyashya.refit.domain.user.dto.request;
 
 import com.shyashyashya.refit.domain.user.constant.UserConstant;
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record MyProfileUpdateRequest(
-        @NotBlank @Size(max = UserConstant.USER_NICKNAME_MAX_LENGTH) String nickname,
+        @Nullable @NotBlank @Size(max = UserConstant.USER_NICKNAME_MAX_LENGTH) String nickname,
 
-        @NotNull Long industryId,
-        @NotNull Long jobCategoryId) {}
+        @Nullable Long industryId,
+        @Nullable Long jobCategoryId) {}
