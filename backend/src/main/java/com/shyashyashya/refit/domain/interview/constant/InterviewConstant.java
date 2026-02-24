dbe651cae58e93993688f9cd0670824bfd13db87
kckc0608 <kckc0608@naver.com>
2026-02-24 21:45:04 +0900

    feat: web client 응답 시간 제한을 3분으로 늘림

--- backend/src/main/java/com/shyashyashya/refit/domain/interview/constant/InterviewConstant.java
+++ backend/src/main/java/com/shyashyashya/refit/domain/interview/constant/InterviewConstant.java
@@ -8,5 +8,5 @@ public final class InterviewConstant {
 
     public static final int RAW_TEXT_MAX_LENGTH = 10_000;
     public static final int KPT_TEXT_MAX_LENGTH = 8_000;
-    public static final long QNA_SET_CONVERT_RESULT_TIMEOUT_MILLISECONDS = 30_000L;
+    public static final long QNA_SET_CONVERT_RESULT_TIMEOUT_MILLISECONDS = 120_000L;
 }
